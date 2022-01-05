292284624b038395780b9af589a798bb1dc3629d
Joseph Parker <joseph.parker@ukaea.uk>
2022-01-05 16:06:59 +0000

    Add MMS test for initial conditions

--- test/mms_test.cc
+++ test/mms_test.cc
@@ -2,57 +2,51 @@
 #include "../src/mesh.hpp"
 #include "../src/plasma.hpp"
 #include <cmath>
+#include <boost/math/statistics/linear_regression.hpp>
 
-TEST(MMSTest, InitialConditions) {
+TEST(MMSTest, SpatialInitialConditions) {
   Mesh mesh;
   Plasma plasma;
 
-  std::vector<double> nparticles;
-  std::vector<double> summed_error;
+  std::vector<double> log_nparticles;
+  std::vector<double> log_summed_error;
   double error;
 
-  // Deposit partilces onto mesh, and test that the resulting
+  // Deposit particles onto mesh, and test that the resulting
   // distribution tends to a cosine.
   int np = 1;
-  for(int j = 0; j < 20; j++){
+
+  // j < 20, 3.6 secs
+  // j < 11, 0.01 secs
+  for(int j = 0; j < 11; j++){
   	np *= 2;
 	Plasma plasma(np);
 
 	mesh.deposit(&plasma);
-	for(int i = 0; i < mesh.nmesh; i++){
-	  std::cout << mesh.charge_density[i] << " ";
-	}
-	std::cout << "\n";
+//	for(int i = 0; i < mesh.nmesh; i++){
+//	  std::cout << mesh.charge_density[i] << " ";
+//	}
+//	std::cout << "\n";
 
 	error = 0.0;
 	for(int i = 0; i < mesh.nmesh; i++){
 	  error += std::abs(mesh.charge_density[i] - (1.0 + 0.01 * cos( 2.0*M_PI*mesh.mesh[i]))/double(mesh.nintervals));
 	}
 
-	nparticles.push_back(np);
-	summed_error.push_back(error);
+	log_nparticles.push_back(std::log(np));
+	log_summed_error.push_back(std::log(error));
   }
+
+
+//  for(int j = 0; j < 20; j++){
+//	  std::cout << log_summed_error[j] << " ";
+//  }
+
+  using boost::math::statistics::simple_ordinary_least_squares;
+  auto [c0, c1] = simple_ordinary_least_squares(log_nparticles, log_summed_error);
+  //std::cout << "f(x) = " << c0 << " + " << c1 << "*x" << "\n";
 
   // summed error should decay like nparticles^{-1/2}
-
-  for(int j = 0; j < 20; j++){
-	  std::cout << summed_error[j] << " ";
-  }
-
-
-  EXPECT_EQ(mesh.t, 0.0);
-  EXPECT_EQ(mesh.dt, 0.01);
-  EXPECT_EQ(mesh.nt, 1000);
-  EXPECT_EQ(mesh.nintervals, 10);
-  EXPECT_EQ(mesh.nmesh, 11);
-  EXPECT_EQ(mesh.mesh.size(), mesh.nmesh);
-  EXPECT_EQ(mesh.dx, 0.1);
-  for(int i = 0; i < mesh.mesh.size(); i++){
-  	EXPECT_EQ(mesh.mesh.at(i), double(i)*mesh.dx);
-  }
-  EXPECT_EQ(mesh.mesh_staggered.size(), mesh.nintervals);
-  for(int i = 0; i < mesh.mesh_staggered.size(); i++){
-  	EXPECT_EQ(mesh.mesh_staggered.at(i), double(i+0.5)*mesh.dx);
-  }
+  ASSERT_NEAR(c1, -0.5, 0.05);
 }
 
