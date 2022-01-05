3a531267c3fedb33db24ad10d95e4028c5bc1f8c
Cook, James <james.cook@ukaea.uk>
2022-01-05 10:59:06 +0000

    moved evolve with fft arg out of main to simulation.cpp

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -13,16 +13,15 @@
 /*
  * Evolve simulation through all timesteps
  */
-void evolve(Mesh *mesh, Plasma *plasma) {
+void evolve(Mesh *mesh, Plasma *plasma, FFT *fft) {
 
   for (int i = 0; i < mesh->nt; i++) {
     plasma->push(mesh);
     mesh->deposit(plasma);
-    mesh->solve_for_potential();
-    mesh->get_electric_field();
+    mesh->solve_for_electric_field_fft(fft);
     // TODO: implement real diagnostics!
     for (int j = 0; j < mesh->nmesh-1; j++){
-    	std::cout << mesh->electric_field_staggered[j] << " ";
+    	std::cout << mesh->electric_field[j] << " ";
     }
     std::cout << "\n";
   };
