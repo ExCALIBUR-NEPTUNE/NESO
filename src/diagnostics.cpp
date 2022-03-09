a8bd9a731569747ab61145043e85e3b3bb8ef163
Joseph Parker <joseph.parker@ukaea.uk>
2022-03-09 17:10:36 +0000

    Working reduction for single value diagnostics

--- src/diagnostics.cpp
+++ src/diagnostics.cpp
@@ -15,6 +15,12 @@
 #include <CL/sycl.hpp>
 #endif
 
+#include <oneapi/dpl/algorithm>
+#include <oneapi/dpl/numeric>
+#include <oneapi/dpl/execution>
+#include <oneapi/dpl/iterator>
+#include "dpc_common.hpp"
+
 
 /*
  * Store simulation time as a vector
@@ -40,14 +46,64 @@ void Diagnostics::compute_total_energy(Mesh &mesh, Plasma &plasma){
  */
 void Diagnostics::compute_field_energy(Mesh &mesh) {
 
-	double energy = 0.0;
-	// NB: Omit periodic point
-	for( std::size_t i = 0; i < mesh.electric_field.size()-1; i++) {
-		energy += std::pow(mesh.electric_field.at(i),2);
-	}
-	energy *= 0.5 / std::pow(mesh.normalized_box_length,2);
+	const int num_steps = mesh.electric_field.size()-1;
+	sycl::queue myQueue{sycl::property::queue::in_order()};
+  auto policy = dpl::execution::make_device_policy(
+      sycl::queue(sycl::default_selector{}, dpc_common::exception_handler));
 
-	field_energy.push_back(energy);
+	//double energy = 0.0;
+//	// NB: Omit periodic point
+//	for( std::size_t i = 0; i < mesh.electric_field.size()-1; i++) {
+//		energy += std::pow(mesh.electric_field.at(i),2);
+//	}
+//	energy *= 0.5 / std::pow(mesh.normalized_box_length,2);
+
+//  auto electric_field_a = mesh.electric_field_d.get_access<sycl::access::mode::read_write>();
+//
+//  double energy = std::transform_reduce(
+//      policy, dpl::counting_iterator<int>(1),
+//      oneapi::dpl::counting_iterator<int>(n), 0.0f, std::plus<double>(),
+//      [=](int i) {
+//        //float x = (float)(((float)i - 0.5f) / (float(num_steps)));
+//	//double x = std::pow(mesh.electric_field.at(i),2);
+//	double x = std::pow(electric_field_a[i],2);
+//        return x;
+//      });
+
+  double data[num_steps];
+
+  // Create buffer using host allocated "data" array
+  sycl::buffer<double, 1> buf{data, sycl::range<1>{size_t(num_steps)}};
+
+  policy.queue().submit([&](sycl::handler& h) {
+		  sycl::accessor writeresult(buf,h,sycl::write_only);
+      auto electric_field_a = mesh.electric_field_d.get_access<sycl::access::mode::read_write>(h);
+    h.parallel_for(sycl::range<1>{size_t(num_steps)}, [=](sycl::id<1> idx) {
+      // float x = ((float)idx[0] - 0.5) / (float)num_steps;
+      double x = std::pow(electric_field_a[idx],2);
+      writeresult[idx[0]] = x;
+    });
+  });
+  policy.queue().wait();
+
+  // Single task is needed here to make sure
+  // data is not written over.
+  policy.queue().submit([&](sycl::handler& h) {
+		  sycl::accessor a(buf,h);
+    h.single_task([=]() {
+      for (int i = 1; i < num_steps; i++) a[0] += a[i];
+    });
+  });
+  policy.queue().wait();
+
+
+  // float mynewresult = buf.get_access<access::mode::read>()[0] / (float)num_steps;
+  sycl::host_accessor answer(buf,sycl::read_only) ; 
+  double mynewresult = answer[0]; 
+
+  double energy = mynewresult * 0.5 / std::pow(mesh.normalized_box_length,2);
+
+  field_energy.push_back(energy);
 }
 
 /*
