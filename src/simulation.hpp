b5a849cee4962f5deacc231ff45b904732f1a728
Joseph Parker <joseph.parker@ukaea.uk>
2022-04-08 11:31:18 +0100

    Some improvements to CMake build (#37)
    * Nearly building, failing on BOOST error
    
    * Find boost, though with hardoded local path
    
    * Enforce c++ standards in cmake
    
    * Fix boost finding
    
    * Add address sanitizers
    
    * Ensure test exe name is set
    
    * Add missing header file
    
    * Add forgotten header
    
    * Remove headers - whoops these have been moved to include
    
    * added extre diagnostic info
    
    * PIE, add sycl to exe, include
    
    * add_sycl_to_target is hipsycl not inteldpcpp
    
    * wrong sycl framework in if for add_sycl_to_target
    
    * if hipsycl O2 -fPIE
    
    Co-authored-by: Cook, James <james.cook@ukaea.uk>
    Co-authored-by: James Cook <cookjws@gmail.com>

--- src/simulation.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "mesh.hpp"
-#include "plasma.hpp"
-#include "diagnostics.hpp"
-
-void initialize();
-void evolve(Mesh *mesh, Plasma *plasma, FFT *fft, Diagnostics *diagnostics);
