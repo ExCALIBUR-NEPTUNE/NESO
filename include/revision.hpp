2a399c1233620d79a10cbf8d27bf1ff85334754c
James Cook <james.cook@ukaea.uk>
2022-05-26 10:48:32 +0000

    re-added revision.hpp but changed default git REVISION and STATUS strings accidentally commited this first to init-combined-solver branch

---
+++ include/revision.hpp
@@ -0,0 +1,28 @@
+/// Information about the version of NESO
+///
+/// The build system will update this file on every commit, which may
+/// result in files that include it getting rebuilt. Therefore it
+/// should be included in as few places as possible
+
+#ifndef NESO_REVISION_H
+#define NESO_REVISION_H
+
+namespace NESO {
+namespace version {
+/// The git commit hash
+#ifndef NESO_REVISION
+constexpr auto revision = "GIT_HASH_NOT_FOUND_BY_CMAKE";
+constexpr auto git_state = "UNKNOWN";
+#else
+// Stringify value passed at compile time
+#define BUILDFLAG1_(x) #x
+#define BUILDFLAG(x) BUILDFLAG1_(x)
+constexpr auto revision = BUILDFLAG(NESO_REVISION);
+constexpr auto git_state = BUILDFLAG(NESO_GIT_STATE);
+#undef BUILDFLAG1
+#undef BUILDFLAG
+#endif
+} // namespace version
+} // namespace NESO
+
+#endif // NESO_REVISION_H
