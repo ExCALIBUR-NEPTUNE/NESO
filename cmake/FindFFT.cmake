bbb6fc7a394b91f96bcfbab07db9d588aa1fe2d3
Chris MacMackin <chris.macmackin@ukaea.uk>
2022-11-07 16:51:14 +0000

    Created separate file for finding FFT implementation
    Also simplified and tidied up the main CMake script quite a bit

---
+++ cmake/FindFFT.cmake
@@ -0,0 +1,42 @@
+# FindFFT.cmake
+#
+# Finds an implementation of the FFTW interface. It will look for
+# Intel MKL and, if that is not available,  fall back to FFTW3.
+#
+# This will define the following variables
+#
+#    FFT_FOUND
+#    FFT_IMPLEMENTATION
+#
+# and the targets
+#
+#     fft::fft
+#
+
+set(FFT_FOUND FALSE)
+
+# Hack to deal with the fact Intel seems to be inconsistent with whether it
+# prefers you to use dpcpp or icpx directly. MKL requires the compiler to be
+# dpcpp, but the DPCPP cmake files actually prefer that icpx is used.
+if(CMAKE_CXX_COMPILER MATCHES "icpx$")
+  set(CMAKE_CXX_COMPILER_ORIGINAL ${CMAKE_CXX_COMPILER})
+  set(CMAKE_CXX_COMPILER "dpcpp")
+endif()
+find_package(MKL CONFIG QUIET)
+if(CMAKE_CXX_COMPILER_ORIGINAL)
+  set(CMAKE_CXX_COMPILER ${CMAKE_CXX_COMPILER_ORIGINAL})
+endif()
+
+if(MKL_FOUND)
+  set(FFT_IMPLEMENTATION "Intel_MKL")
+  set(FFT_FOUND TRUE)
+  add_library(fft::fft ALIAS MKL::MKL_DPCPP)
+else()
+  find_package(PkgConfig REQUIRED)
+  pkg_search_module(FFTW QUIET fftw3 IMPORTED_TARGET)
+  if(FFTW_FOUND)
+    set(FFT_IMPLEMENTATION fftw)
+    set(FFT_FOUND TRUE)
+    add_library(fft::fft ALIAS PkgConfig::FFTW)
+  endif()
+endif()
