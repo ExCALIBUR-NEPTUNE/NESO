f4e75ffeb4aea82cff69708184bbd7fa9e9a4de3
Chris MacMackin <chris.macmackin@ukaea.uk>
2022-11-09 15:02:33 +0000

    Ensured build flags are actually used

--- cmake/FindNektar++.cmake
+++ cmake/FindNektar++.cmake
@@ -16,9 +16,6 @@
 
 find_package(Nektar++ CONFIG)
 
-message(" ${NEKTAR++_LIBRARIES}")
-message(" ${ABSOLUTE_NEKTAR++_LIBRARIES}")
-
 if(Nektar++_FOUND AND NOT TARGET Nektar++::nektar++)
   set(NEKTAR++_TP_FILTERED_INCLUDE_DIRS ${NEKTAR++_TP_INCLUDE_DIRS})
   list(REMOVE_ITEM NEKTAR++_TP_FILTERED_INCLUDE_DIRS "include/nektar++")
