9086da065da021cba68562e203cf033db203a0d3
Will Saunders <will.saunders@ukaea.uk>
2022-11-30 14:22:45 +0000

    implemented pydata based docs like NESO-Particles

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -9,28 +9,54 @@
 project = 'NESO'
 copyright = '2022, UKAEA'
 author = 'UKAEA'
-release = '0.0.1'
+release = ''
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["breathe"]
 
-templates_path = ['_templates']
+#templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
 
+html_sidebars = {
+    "**": ["globaltoc.html"]
+}
+
+html_theme_options = {
+    "navbar_start": ["navbar-logo"],
+    "navbar_align": "left",
+    "primary_sidebar_end": [],
+    "navigation_depth": 0,
+    "show_nav_level": 3
+}
+
+import os
+docs_version = "./docs_version"
+if os.path.exists(docs_version):
+
+    with open(docs_version) as fh:
+        version = fh.read().strip()
+
+    html_theme_options.update({
+        "check_switcher": False,
+        "switcher": {
+            "json_url": "https://excalibur-neptune.github.io/NESO/switcher.json",
+            "version_match": version,
+        },
+        "navbar_start": ["navbar-logo", "version-switcher"]
+    })
 
 breathe_projects = {"NESO": "../../build/doxygen/xml"}
 breathe_default_project = "NESO"
 
-
-
+# Enable referencing figures etc by number.
+numfig = True
 
