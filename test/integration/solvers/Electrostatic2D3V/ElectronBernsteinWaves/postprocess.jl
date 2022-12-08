727df196eab31f3a872abe55933564be22db3f51
James Cook <James Cook>
2022-12-08 16:45:25 +0000

    update ebw init conds and post processing script

--- test/integration/solvers/Electrostatic2D3V/ElectronBernsteinWaves/postprocess.jl
+++ test/integration/solvers/Electrostatic2D3V/ElectronBernsteinWaves/postprocess.jl
@@ -2,8 +2,8 @@ using HDF5, Makie, CairoMakie, FFTW
 
 fname = "Electrostatic2D3V_line_field_deriv_evaluations.h5part"
 
-E1 = hcat([h5read(fname, "Step#$i/FIELD_DERIV_EVALUATION_1") for i in (0, 16:16:1023...)]...);
-E0 = hcat([h5read(fname, "Step#$i/FIELD_DERIV_EVALUATION_0") for i in (0, 16:16:1023...)]...);
+E1 = hcat([h5read(fname, "Step#$i/FIELD_DERIV_EVALUATION_1") for i in (0, 16:16:4095...)]...);
+E0 = hcat([h5read(fname, "Step#$i/FIELD_DERIV_EVALUATION_0") for i in (0, 16:16:4095...)]...);
 
 for (field, string) in ((E0, "E0"), (E1, "E1"))
   fig = Figure(; resolution=(600, 400))
