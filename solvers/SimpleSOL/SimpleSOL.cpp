9d2aa2e942423e9c9c41aa4ca70b8c03fae4888f
Owen Parry <owen.parry@ukaea.uk>
2022-12-19 16:40:14 +0000

    Reorganise SimpleSOL exec to use compflow-solver-lib.

--- solvers/SimpleSOL/SimpleSOL.cpp
+++ solvers/SimpleSOL/SimpleSOL.cpp
@@ -1,45 +1,23 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-// File: SOLSolver.cpp
+// File: SimpleSOL.cpp
 //
-// For more information, please see: http://www.nektar.info
 //
-// The MIT License
-//
-// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
-// Department of Aeronautics, Imperial College London (UK), and Scientific
-// Computing and Imaging Institute, University of Utah (USA).
-//
-// License for the specific language governing rights and limitations under
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-//
-// Description: Driver for the SOL solver.
+// Description: Driver for SimpleSOL.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <LibUtilities/BasicUtils/SessionReader.h>
 #include <SolverUtils/Driver.h>
 
+#include "SimpleSOL.h"
+
 using namespace Nektar;
 using namespace Nektar::SolverUtils;
 
-int main(int argc, char *argv[]) {
+namespace NESO {
+namespace Solvers {
+int run_SimpleSOL(int argc, char *argv[]) {
   try {
     // Create session reader.
     auto session = LibUtilities::SessionReader::CreateInstance(argc, argv);
@@ -65,3 +43,6 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+
+} // namespace Solvers
+} // namespace NESO
