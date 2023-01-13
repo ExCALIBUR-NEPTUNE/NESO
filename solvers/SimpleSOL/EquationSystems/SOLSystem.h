e6e13283a95d674a00506f7c485121f83fb88bd9
oparry-ukaea <101191772+oparry-ukaea@users.noreply.github.com>
2023-01-13 16:32:05 +0000

    Nektar SOL solver (#116)
    * Copied SimpleSOL source from https://github.com/oparry-ukaea/nektar-simple-sol @ 70125a46bbf18d3cb2bf7250f7ce1d656506e8a8.
    
    * Reorganised SimpleSOL source.
    
    * Added 1D and 2D examples for the SimpleSOL solver; no coupling yet.
    
    * Ignore CMake cache files.
    
    * Added a script to simplify running examples.
    
    * Formatted solvers/SimpleSOL/CMakeLists.txt with cmake-format.
    
    * Add SimpleSOL (and any other solvers) as subdirectories in the top-level CmakeLists.
    
    * Corrected solver exec path in run_eg script.
    
    * Ignore the VSCode settings directory.
    
    * Removed / simplifiied some includes.
    
    * Remove source files duplicated from Nektar and link to the built object files directly.
    
    * Removed .github/workflows/neso.yaml
    
    * Reorganise SimpleSOL exec to use compflow-solver-lib.
    
    * Add override to prevent output of solution.txt.
    
    * Build object library from SimpleSOL source for use with tests.
    
    * Added a gtest fixture for Nektar solver testing.
    
    * Added a gtest fixture for testing SimpleSOL.
    
    * Added a 1D test for SimpleSOL.
    
    * Added a 2D test for SimpleSOL.
    
    * Define the integration test executable as a sycl target.
    
    * Ran cmake-format on solvers/CMakeLists.txt .
    
    * Updated NESO-Spack to include Nektar Compressible Flow Solver library build.
    
    * Make SimpleSOL tests work with MPI.
    
    * Corrected exception handling in run_SimpleSOL.
    
    * Corrected handling of custom args in Nektar solver tests.
    
    * Propagate compile options to integration test exec.
    
    * Updated NESO-Spack version to ensure gmock gets built.
    
    * Fixed the run_eg script - look for solver exec in most recently modified build directory; override default with -b.
    
    * Document solver directory structure and run_eg script.
    
    Co-authored-by: Will Saunders <will.saunders@ukaea.uk>

---
+++ solvers/SimpleSOL/EquationSystems/SOLSystem.h
@@ -0,0 +1,163 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File SOLSystem.h
+//
+// For more information, please see: http://www.nektar.info
+//
+// The MIT License
+//
+// Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+// Department of Aeronautics, Imperial College London (UK), and Scientific
+// Computing and Imaging Institute, University of Utah (USA).
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// Description: Auxiliary functions for the 1D SOL system
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef SOLSYSTEM_H
+#define SOLSYSTEM_H
+
+#include <boost/core/ignore_unused.hpp>
+
+#include <LocalRegions/Expansion2D.h>
+#include <LocalRegions/Expansion3D.h>
+#include <MultiRegions/GlobalMatrixKey.h>
+#include <SolverUtils/AdvectionSystem.h>
+#include <SolverUtils/Diffusion/Diffusion.h>
+#include <SolverUtils/Filters/FilterInterfaces.hpp>
+#include <SolverUtils/Forcing/Forcing.h>
+#include <SolverUtils/RiemannSolvers/RiemannSolver.h>
+#include <SolverUtils/UnsteadySystem.h>
+
+#include <CompressibleFlowSolver/Misc/VariableConverter.h>
+
+namespace Nektar {
+/**
+ *
+ */
+class SOLSystem : public SolverUtils::AdvectionSystem,
+                  public SolverUtils::FluidInterface {
+public:
+  friend class MemoryManager<SOLSystem>;
+
+  /// Creates an instance of this class.
+  static SolverUtils::EquationSystemSharedPtr
+  create(const LibUtilities::SessionReaderSharedPtr &pSession,
+         const SpatialDomains::MeshGraphSharedPtr &pGraph) {
+    SolverUtils::EquationSystemSharedPtr p =
+        MemoryManager<SOLSystem>::AllocateSharedPtr(pSession, pGraph);
+    p->InitObject();
+    return p;
+  }
+
+  /// Name of class.
+  static std::string className;
+
+  virtual ~SOLSystem();
+
+  /// Function to get estimate of min h/p factor per element
+  Array<OneD, NekDouble> GetElmtMinHP(void);
+
+  virtual void
+  GetPressure(const Array<OneD, const Array<OneD, NekDouble>> &physfield,
+              Array<OneD, NekDouble> &pressure);
+
+  virtual void
+  GetDensity(const Array<OneD, const Array<OneD, NekDouble>> &physfield,
+             Array<OneD, NekDouble> &density);
+
+  virtual bool HasConstantDensity() { return false; }
+
+  virtual void
+  GetVelocity(const Array<OneD, const Array<OneD, NekDouble>> &physfield,
+              Array<OneD, Array<OneD, NekDouble>> &velocity);
+
+protected:
+  SolverUtils::DiffusionSharedPtr m_diffusion;
+  Array<OneD, Array<OneD, NekDouble>> m_vecLocs;
+  NekDouble m_gamma;
+
+  // Auxiliary object to convert variables
+  VariableConverterSharedPtr m_varConv;
+
+  NekDouble m_bndEvaluateTime;
+
+  // Forcing term
+  std::vector<SolverUtils::ForcingSharedPtr> m_forcing;
+
+  SOLSystem(const LibUtilities::SessionReaderSharedPtr &pSession,
+            const SpatialDomains::MeshGraphSharedPtr &pGraph);
+
+  virtual void v_InitObject(bool DeclareField) override;
+
+  void InitAdvection();
+
+  void DoOdeRhs(const Array<OneD, const Array<OneD, NekDouble>> &inarray,
+                Array<OneD, Array<OneD, NekDouble>> &outarray,
+                const NekDouble time);
+  void DoOdeProjection(const Array<OneD, const Array<OneD, NekDouble>> &inarray,
+                       Array<OneD, Array<OneD, NekDouble>> &outarray,
+                       const NekDouble time);
+
+  void DoAdvection(const Array<OneD, const Array<OneD, NekDouble>> &inarray,
+                   Array<OneD, Array<OneD, NekDouble>> &outarray,
+                   const NekDouble time,
+                   const Array<OneD, const Array<OneD, NekDouble>> &pFwd,
+                   const Array<OneD, const Array<OneD, NekDouble>> &pBwd);
+
+  void DoDiffusion(const Array<OneD, const Array<OneD, NekDouble>> &inarray,
+                   Array<OneD, Array<OneD, NekDouble>> &outarray,
+                   const Array<OneD, const Array<OneD, NekDouble>> &pFwd,
+                   const Array<OneD, const Array<OneD, NekDouble>> &pBwd);
+
+  void GetFluxVector(const Array<OneD, const Array<OneD, NekDouble>> &physfield,
+                     TensorOfArray3D<NekDouble> &flux);
+
+  void SetBoundaryConditions(Array<OneD, Array<OneD, NekDouble>> &physarray,
+                             NekDouble time);
+
+  void GetElmtTimeStep(const Array<OneD, const Array<OneD, NekDouble>> &inarray,
+                       Array<OneD, NekDouble> &tstep);
+
+  virtual NekDouble v_GetTimeStep(
+      const Array<OneD, const Array<OneD, NekDouble>> &inarray) override;
+
+  NekDouble GetGamma() { return m_gamma; }
+
+  const Array<OneD, const Array<OneD, NekDouble>> &GetVecLocs() {
+    return m_vecLocs;
+  }
+
+  const Array<OneD, const Array<OneD, NekDouble>> &GetNormals() {
+    return m_traceNormals;
+  }
+
+  virtual Array<OneD, NekDouble>
+  v_GetMaxStdVelocity(const NekDouble SpeedSoundFactor) override;
+
+  virtual void v_SteadyStateResidual(int step,
+                                     Array<OneD, NekDouble> &L2) override;
+
+  virtual void v_AppendOutput1D(Array<OneD, Array<OneD, NekDouble>> &solution1D) override{};
+};
+
+} // namespace Nektar
+#endif
