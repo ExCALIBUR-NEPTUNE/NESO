fac5b364e1c105bf1a4e2989718976db77a79495
Will Saunders <will.saunders@ukaea.uk>
2023-01-26 13:54:13 +0000

    reimplemented projection for multiple destination functions and sources

--- include/nektar_interface/function_projection.hpp
+++ include/nektar_interface/function_projection.hpp
@@ -123,18 +123,30 @@ public:
 
     if (this->fields.size() > 1) {
       // check all the fields have the same properties
+
+      const int ncoeffs = this->fields[0]->GetNcoeffs();
+      const int npoints = this->fields[0]->GetTotPoints();
+
       for (auto &fieldx : this->fields) {
         auto test_expansion = fieldx->GetExp();
 
+        NESOASSERT(fieldx->GetNcoeffs() == ncoeffs, "Missmatch of ncoeffs.");
+
+        NESOASSERT(fieldx->GetTotPoints() == npoints, "Missmatch of npoints.");
+
         NESOASSERT(test_expansion->size() == num_expansions,
                    "Missmatch of expansion count between fields.");
+
         for (int ex = 0; ex < num_expansions; ex++) {
           const int ndim = (*expansions)[0]->GetCoordim();
+
           NESOASSERT((*test_expansion)[0]->GetCoordim() == ndim,
                      "Missmatch of field dimension count.");
+
           NESOASSERT((*test_expansion)[ex]->GetGeom()->GetGlobalID() ==
                          (*expansions)[ex]->GetGeom()->GetGlobalID(),
-                     "Mesh missmatch between fields.");
+                     "Mesh missmatch between fields (Geom IDs).");
+
           for (int dx = 0; dx < ndim; dx++) {
             NESOASSERT((*test_expansion)[ex]->GetBasisNumModes(dx) ==
                            (*expansions)[ex]->GetBasisNumModes(dx),
@@ -142,7 +154,7 @@ public:
 
             NESOASSERT((*test_expansion)[ex]->GetBasis(dx)->GetBasisType() ==
                            (*expansions)[ex]->GetBasis(dx)->GetBasisType(),
-                       "Basis missmatch between fields.");
+                       "Basis type missmatch between fields.");
           }
         }
       }
@@ -150,7 +162,17 @@ public:
   };
 
   /**
-   *  TODO
+   * Project the particle data from the given ParticleDats onto the Nektar++
+   * fields. It is assumed that the reference positions of particles have aleady
+   * been computed and are stored on the particles. This reference position
+   * computation is performed as particle of the cell binning process
+   * implemented in NektarGraphLocalMapperT.
+   *
+   * @param syms Vector of ParticleDats in the ParticleGroup to use as the
+   * particle weights.
+   * @param components Vector of components to index into the ParticleDats, i.e.
+   * if the ParticleDat has two components a 1 in this vector extracts the
+   * second component.
    */
   template <typename U>
   inline void project(std::vector<Sym<U>> syms, std::vector<int> components) {
@@ -195,6 +217,7 @@ public:
       const int ncol = dat_tmp->ncomp;
       NESOASSERT((0 <= components[symx]) && (components[symx] < ncol),
                  "Component to project out of range.");
+
       // allocate space to store the particle values
       input_tmp.push_back(
           std::make_unique<CellDataT<U>>(this->sycl_target, nrow_max, ncol));
@@ -202,10 +225,10 @@ public:
       // allocate space to store the RHS values of the projection
       global_phi.push_back(std::make_unique<Array<OneD, NekDouble>>(ncoeffs));
     }
-    
+
     // zero the output arrays
-    for(int fieldx=0 ; fieldx<nfields ; fieldx++){
-      for(int cx=0 ; cx<ncoeffs ; cx++){
+    for (int fieldx = 0; fieldx < nfields; fieldx++) {
+      for (int cx = 0; cx < ncoeffs; cx++) {
         (*global_phi[fieldx])[cx] = 0.0;
       }
     }
@@ -240,7 +263,7 @@ public:
       NESOASSERT(this->geom_to_exp.count(nektar_geom_id),
                  "Could not find expansion id for geom id");
       const int nektar_expansion_id = this->geom_to_exp[nektar_geom_id];
-      const int coordim = this->field->GetCoordim(nektar_expansion_id);
+      const int coordim = this->fields[0]->GetCoordim(nektar_expansion_id);
       NESOASSERT(particle_ndim >= coordim, "mismatch in coordinate size");
 
       // Get the expansion object that corresponds to the first expansion
@@ -248,7 +271,14 @@ public:
       // get the number of modes in this expansion
       const int num_modes = nektar_expansion_0->GetNcoeffs();
       // get the offset in the expansion values for this mesh cell
-      const auto expansion_offset = this->field->GetCoeff_Offset(nektar_expansion_id);
+      const auto expansion_offset =
+          this->fields[0]->GetCoeff_Offset(nektar_expansion_id);
+
+      for (int fieldx = 0; fieldx < nfields; fieldx++) {
+        NESOASSERT(this->fields[fieldx]->GetCoeff_Offset(nektar_expansion_id) ==
+                       expansion_offset,
+                   "Missmatch in expansion offset.");
+      }
 
       // wait for the copy of particle data to host
       event_stack.wait();
@@ -267,25 +297,24 @@ public:
           const double phi_j =
               nektar_expansion_0->PhysEvaluateBasis(local_coord, modex);
           // for each field reuse the computed basis function value
-          for(int fieldx=0 ; fieldx<nfields ; fieldx++){
+          for (int fieldx = 0; fieldx < nfields; fieldx++) {
             const int componentx = components[fieldx];
             // read the component from the particle for the nth field
-            const auto quantity = input_tmp[fieldx][componentx][rowx];
+            const auto quantity = (*input_tmp[fieldx])[componentx][rowx];
 
             // offset to this dof in this field
             auto phi = (*global_phi[fieldx]) + expansion_offset;
             phi[modex] += phi_j * quantity;
           }
         }
-
       }
     }
 
     // solve mass matrix system to do projections
     Array<OneD, NekDouble> global_coeffs = Array<OneD, NekDouble>(ncoeffs);
-    const int tot_points = this->field[0]->GetTotPoints();
+    const int tot_points = this->fields[0]->GetTotPoints();
     Array<OneD, NekDouble> global_phys(tot_points);
-    for(int fieldx=0 ; fieldx<nfields ; fieldx++){
+    for (int fieldx = 0; fieldx < nfields; fieldx++) {
       for (int cx = 0; cx < ncoeffs; cx++) {
         NESOASSERT(std::isfinite((*global_phi[fieldx])[cx]),
                    "A projection RHS value is nan.");
@@ -293,23 +322,23 @@ public:
       }
 
       // Solve the mass matrix system
-      multiply_by_inverse_mass_matrix(this->fields[fieldx], *global_phi[fieldx], global_coeffs);
+      multiply_by_inverse_mass_matrix(this->fields[fieldx], *global_phi[fieldx],
+                                      global_coeffs);
 
       for (int cx = 0; cx < ncoeffs; cx++) {
         NESOASSERT(std::isfinite(global_coeffs[cx]),
                    "A projection LHS value is nan.");
         // set the coefficients on the function
-        this->field[fieldx]->SetCoeff(cx, global_coeffs[cx]);
+        this->fields[fieldx]->SetCoeff(cx, global_coeffs[cx]);
       }
-      // set the values at the quadrature points of the function to correspond to
-      // the DOFs we just computed.
+      // set the values at the quadrature points of the function to correspond
+      // to the DOFs we just computed.
       for (int cx = 0; cx < tot_points; cx++) {
         global_phys[cx] = 0.0;
       }
-      this->field->BwdTrans(global_coeffs, global_phys);
-      this->field->SetPhys(global_phys);
+      this->fields[fieldx]->BwdTrans(global_coeffs, global_phys);
+      this->fields[fieldx]->SetPhys(global_phys);
     }
-
   }
 
   /**
@@ -322,109 +351,9 @@ public:
    * @param sym ParticleDat in the ParticleGroup to use as the particle weights.
    */
   template <typename U> inline void project(Sym<U> sym) {
-
-    auto input_dat = (*this->particle_group)[sym];
-    auto ref_position_dat =
-        (*this->particle_group)[Sym<REAL>("NESO_REFERENCE_POSITIONS")];
-
-    const int nrow_max =
-        this->particle_group->mpi_rank_dat->cell_dat.get_nrow_max();
-    const int ncol = input_dat->ncomp;
-    const int particle_ndim = ref_position_dat->ncomp;
-
-    Array<OneD, NekDouble> local_coord(particle_ndim);
-
-    const int ncoeffs = this->field->GetNcoeffs();
-    Array<OneD, NekDouble> global_coeffs = Array<OneD, NekDouble>(ncoeffs);
-    Array<OneD, NekDouble> global_phi = Array<OneD, NekDouble>(ncoeffs);
-
-    CellDataT<U> input_tmp(this->sycl_target, nrow_max, ncol);
-    CellDataT<REAL> ref_positions_tmp(this->sycl_target, nrow_max,
-                                      particle_ndim);
-    EventStack event_stack;
-
-    const int neso_cell_count =
-        this->particle_group->domain->mesh->get_cell_count();
-    for (int neso_cellx = 0; neso_cellx < neso_cell_count; neso_cellx++) {
-      // Get the source values.
-      input_dat->cell_dat.get_cell_async(neso_cellx, input_tmp, event_stack);
-      // Get the reference positions from the particle in the cell
-      ref_position_dat->cell_dat.get_cell_async(neso_cellx, ref_positions_tmp,
-                                                event_stack);
-
-      // Get the nektar++ geometry id that corresponds to this NESO cell id
-      const int nektar_geom_id =
-          this->cell_id_translation->map_to_nektar[neso_cellx];
-
-      // Map from the geometry id to the expansion id for the field.
-      NESOASSERT(this->geom_to_exp.count(nektar_geom_id),
-                 "Could not find expansion id for geom id");
-      const int nektar_expansion_id = this->geom_to_exp[nektar_geom_id];
-      const int coordim = this->field->GetCoordim(nektar_expansion_id);
-      NESOASSERT(particle_ndim >= coordim, "mismatch in coordinate size");
-
-      // Get the expansion object that corresponds to this expansion id
-      auto nektar_expansion = this->field->GetExp(nektar_expansion_id);
-
-      // get the number of modes in this expansion
-      const int num_modes = nektar_expansion->GetNcoeffs();
-
-      auto phi = global_phi + this->field->GetCoeff_Offset(nektar_expansion_id);
-
-      // zero the output array
-      for (int modex = 0; modex < num_modes; modex++) {
-        phi[modex] = 0.0;
-      }
-
-      // wait for the copy of particle data to host
-      event_stack.wait();
-      const int nrow = input_dat->cell_dat.nrow[neso_cellx];
-      // for each particle in the cell
-      for (int rowx = 0; rowx < nrow; rowx++) {
-
-        const REAL quantity = input_tmp[0][rowx];
-
-        // read the reference position from the particle
-        for (int dimx = 0; dimx < particle_ndim; dimx++) {
-          local_coord[dimx] = ref_positions_tmp[dimx][rowx];
-        }
-
-        // for each mode in the expansion evaluate the basis function
-        // corresponding to that node at the location of the particle, then
-        // re-weight with the value on the particle.
-        for (int modex = 0; modex < num_modes; modex++) {
-          const double phi_j =
-              nektar_expansion->PhysEvaluateBasis(local_coord, modex);
-          phi[modex] += phi_j * quantity;
-        }
-      }
-    }
-
-    for (int cx = 0; cx < ncoeffs; cx++) {
-      NESOASSERT(std::isfinite(global_phi[cx]),
-                 "A projection RHS value is nan.");
-      global_coeffs[cx] = 0.0;
-    }
-
-    // Solve the mass matrix system
-    multiply_by_inverse_mass_matrix(this->field, global_phi, global_coeffs);
-
-    for (int cx = 0; cx < ncoeffs; cx++) {
-      NESOASSERT(std::isfinite(global_coeffs[cx]),
-                 "A projection LHS value is nan.");
-      // set the coefficients on the function
-      this->field->SetCoeff(cx, global_coeffs[cx]);
-    }
-
-    // set the values at the quadrature points of the function to correspond to
-    // the DOFs we just computed.
-    const int tot_points = this->field->GetTotPoints();
-    Array<OneD, NekDouble> global_phys(tot_points);
-    for (int cx = 0; cx < tot_points; cx++) {
-      global_phys[cx] = 0.0;
-    }
-    this->field->BwdTrans(global_coeffs, global_phys);
-    this->field->SetPhys(global_phys);
+    std::vector<Sym<U>> syms = {sym};
+    std::vector<int> components = {0};
+    this->project(syms, components);
   }
 };
 
