0c758d62a5c683ee9b53d90721b0c6aab03e3290
Owen Parry <owen.parry@ukaea.uk>
2023-02-13 08:40:16 +0000

    Look for sources stored in named fields (called [field]_src) and add them as SOL forcing terms.

--- solvers/SimpleSOL/Forcing/SourceTerms.cpp
+++ solvers/SimpleSOL/Forcing/SourceTerms.cpp
@@ -113,6 +113,18 @@ void SourceTerms::v_Apply(
   for (int i = 0; i < outarray[E_idx].size(); ++i) {
     outarray[E_idx][i] += CalcGaussian(m_E_prefac, m_mu, m_sigma, m_x[i]) / 2.0;
   }
+
+  // Add sources stored as separate fields, if they exist
+  std::vector<std::string> target_fields = {"rho", "rhou", "E"};
+  for (auto target_field : target_fields) {
+    int src_field_idx = this->field_to_index.get_idx(target_field + "_src");
+    if (src_field_idx >= 0) {
+      int dst_field_idx = this->field_to_index.get_idx(target_field);
+      for (int i = 0; i < outarray[dst_field_idx].size(); ++i) {
+        outarray[dst_field_idx][i] += inarray[src_field_idx][i];
+      }
+    }
+  }
 }
 
 } // namespace Nektar
