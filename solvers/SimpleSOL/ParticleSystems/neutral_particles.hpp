5c891e15f278b99f3e687146415f0e9058b65074
Owen Parry <101191772+oparry-ukaea@users.noreply.github.com>
2023-03-07 10:06:48 +0000

    Project particle momentum and energy (#159)
    * Add momentum source fields.
    
    * Setup projection onto rhou_src, rhov_src. Particle props not populated yet.
    
    * Populate particle SOURCE_MOMENTUM values in the ionisation kernel.
    
    * Add energy source field.
    
    * Compute energy source field on the particle side.
    
    * Correct an index in the ionisation kernel.
    
    * deltaweight cannot be const
    
    ---------
    
    Co-authored-by: James Cook <cookjws@gmail.com>
    Co-authored-by: James Cook <james.cook@ukaea.uk>

--- solvers/SimpleSOL/ParticleSystems/neutral_particles.hpp
+++ solvers/SimpleSOL/ParticleSystems/neutral_particles.hpp
@@ -76,6 +76,7 @@ protected:
   int source_line_bin_count;
   double source_line_offset;
   double particle_thermal_velocity;
+  double theta;
   std::mt19937 rng_phasespace;
   std::normal_distribution<> velocity_normal_distribution;
   std::vector<std::shared_ptr<ParticleInitialisationLine>> source_lines;
@@ -92,7 +93,7 @@ protected:
   std::shared_ptr<FieldEvaluate<DisContField>> field_evaluate_T;
 
   int debug_write_fields_count;
-  std::shared_ptr<DisContField> debug_write_field;
+  std::map<std::string, std::shared_ptr<DisContField>> debug_write_fields;
 
 public:
   /// Disable (implicit) copies.
@@ -183,7 +184,8 @@ public:
         ParticleProp(Sym<INT>("PARTICLE_ID"), 2),
         ParticleProp(Sym<REAL>("COMPUTATIONAL_WEIGHT"), 1),
         ParticleProp(Sym<REAL>("SOURCE_DENSITY"), 1),
-        ParticleProp(Sym<REAL>("SOURCE_MOMENTUM"), 1),
+        ParticleProp(Sym<REAL>("SOURCE_ENERGY"), 1),
+        ParticleProp(Sym<REAL>("SOURCE_MOMENTUM"), 2),
         ParticleProp(Sym<REAL>("ELECTRON_DENSITY"), 1),
         ParticleProp(Sym<REAL>("ELECTRON_TEMPERATURE"), 1),
         ParticleProp(Sym<REAL>("MASS"), 1),
@@ -229,6 +231,7 @@ public:
                      this->source_line_offset, 0.2);
     get_from_session(this->session, "particle_thermal_velocity",
                      this->particle_thermal_velocity, 1.0);
+    get_from_session(this->session, "theta", this->theta, 0.0);
 
     // get seed from file
     std::srand(std::time(nullptr));
@@ -311,13 +314,22 @@ public:
   /**
    * Setup the projection object to use the following fields.
    *
-   * @param rho_src Nektar++ field to project ionised particle data onto.
+   * @param rho_src Nektar++ fields to project ionised particle data onto.
    */
-  inline void setup_project(std::shared_ptr<DisContField> rho_src) {
-    std::vector<std::shared_ptr<DisContField>> fields = {rho_src};
+  inline void setup_project(std::shared_ptr<DisContField> rho_src,
+                            std::shared_ptr<DisContField> rhou_src,
+                            std::shared_ptr<DisContField> rhov_src,
+                            std::shared_ptr<DisContField> E_src) {
+    std::vector<std::shared_ptr<DisContField>> fields = {rho_src, rhou_src,
+                                                         rhov_src, E_src};
     this->field_project = std::make_shared<FieldProject<DisContField>>(
         fields, this->particle_group, this->cell_id_translation);
-    this->debug_write_field = rho_src;
+
+    // Setup debugging output for each field
+    this->debug_write_fields["rho_src"] = rho_src;
+    this->debug_write_fields["rhou_src"] = rhou_src;
+    this->debug_write_fields["rhov_src"] = rhov_src;
+    this->debug_write_fields["E_src"] = E_src;
   }
 
   /**
@@ -348,8 +360,10 @@ public:
     NESOASSERT(this->field_project != nullptr,
                "Field project object is null. Was setup_project called?");
 
-    std::vector<Sym<REAL>> syms = {Sym<REAL>("SOURCE_DENSITY")};
-    std::vector<int> components = {0};
+    std::vector<Sym<REAL>> syms = {
+        Sym<REAL>("SOURCE_DENSITY"), Sym<REAL>("SOURCE_MOMENTUM"),
+        Sym<REAL>("SOURCE_MOMENTUM"), Sym<REAL>("SOURCE_ENERGY")};
+    std::vector<int> components = {0, 0, 1, 0};
     this->field_project->project(syms, components);
   }
 
@@ -437,9 +451,12 @@ public:
    *  Write the projection fields to vtu for debugging.
    */
   inline void write_source_fields() {
-    std::string filename =
-        "debug_" + std::to_string(this->debug_write_fields_count++) + ".vtu";
-    write_vtu(this->debug_write_field, filename);
+    for (auto entry : this->debug_write_fields) {
+      std::string filename = "debug_" + entry.first + "_" +
+                             std::to_string(this->debug_write_fields_count++) +
+                             ".vtu";
+      write_vtu(entry.second, filename);
+    }
   }
 
   /**
@@ -656,7 +673,11 @@ public:
     const double k_rate_factor =
         -k_q_i * 6.7e7 * k_a_i * 1e-6; // 1e-6 to go from cm^3 to m^3
 
+    auto k_cos_theta = std::cos(this->theta);
+    auto k_sin_theta = std::sin(this->theta);
+
     const INT k_remove_key = this->particle_remove_key;
+
     auto t0 = profile_timestamp();
 
     auto k_ID =
@@ -667,6 +688,12 @@ public:
                      ->cell_dat.device_ptr();
     auto k_SD = (*this->particle_group)[Sym<REAL>("SOURCE_DENSITY")]
                     ->cell_dat.device_ptr();
+    auto k_SE = (*this->particle_group)[Sym<REAL>("SOURCE_ENERGY")]
+                    ->cell_dat.device_ptr();
+    auto k_SM = (*this->particle_group)[Sym<REAL>("SOURCE_MOMENTUM")]
+                    ->cell_dat.device_ptr();
+    auto k_V =
+        (*this->particle_group)[Sym<REAL>("VELOCITY")]->cell_dat.device_ptr();
     auto k_W = (*this->particle_group)[Sym<REAL>("COMPUTATIONAL_WEIGHT")]
                    ->cell_dat.device_ptr();
 
@@ -707,8 +734,22 @@ public:
                   k_ID[cellx][0][layerx] = k_remove_key;
                   deltaweight = -weight;
                 }
+                // Mutate the weight on the particle
                 k_W[cellx][0][layerx] += deltaweight;
+                // Set value for fluid density source
                 k_SD[cellx][0][layerx] = -deltaweight;
+
+                // Compute velocity along the SimpleSOL problem axis.
+                // (No momentum coupling in orthogonal dimensions)
+                const REAL v_s = k_V[cellx][0][layerx] * k_cos_theta +
+                                 k_V[cellx][1][layerx] * k_sin_theta;
+                // Set value for fluid momentum density source
+                k_SM[cellx][0][layerx] =
+                    k_SD[cellx][0][layerx] * v_s * k_cos_theta;
+                k_SM[cellx][1][layerx] =
+                    k_SD[cellx][0][layerx] * v_s * k_sin_theta;
+                // Set value for fluid energy source
+                k_SE[cellx][0][layerx] = k_SD[cellx][0][layerx] * v_s * v_s / 2;
                 NESO_PARTICLES_KERNEL_END
               });
         })
