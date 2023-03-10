aa04dae5e750c6120e20a2d5b9146667bc45b57d
Chris MacMackin <chris.macmackin@ukaea.uk>
2023-03-10 19:14:17 +0000

    Added some notes about things yet to do

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -16,6 +16,7 @@ PYBIND11_MODULE(PyNESO, m) {
   m.attr("revision") = NESO::version::revision;
   m.attr("git_state") = NESO::version::git_state;
 
+  // TODO: Provide access to other overloads
   py::class_<sycl::queue>(m, "Queue")
     .def(py::init<>());
 
@@ -31,8 +32,10 @@ PYBIND11_MODULE(PyNESO, m) {
     .def_readonly("dt", &Mesh::dt)
     .def_readonly("nt", &Mesh::nt)
     .def_readonly("nintervals", &Mesh::nintervals)
+    // TODO: Make it possible to convert between numpy arrays and std::vector
     .def_readonly("mesh", &Mesh::mesh);
 
+  // TODO: Make it possible to convert between numpy arrays and std::vector
   py::class_<Velocity>(m, "Velocity")
     .def(py::init<std::vector<double>, std::vector<double>, std::vector<double>>())
     .def_readwrite("x", &Velocity::x)
@@ -45,6 +48,7 @@ PYBIND11_MODULE(PyNESO, m) {
          py::arg("m")=1.0, py::arg("n")=10)
     .def("push", &Species::push)
     .def("set_array_dimensions", &Species::set_array_dimensions)
+    // TODO: Make it possible to convert between numpy arrays and std::vector
     .def("set_initial_conditions", &Species::set_initial_conditions);
 
   py::class_<Plasma>(m, "Plasma")
@@ -52,10 +56,9 @@ PYBIND11_MODULE(PyNESO, m) {
     .def("push", &Plasma::push);
 
   py::class_<FFT>(m, "FFT")
-    .def(py::init<sycl::queue &, int>())
-    .def("forward", &FFT::forward)
-    .def("backward", &FFT::backward);
+    .def(py::init<sycl::queue &, int>());
 
+  // TODO: Make it possible to convert between numpy arrays and std::vector
   py::class_<Diagnostics>(m, "Diagnostics")
     .def(py::init())
     .def_readonly("time", &Diagnostics::time)
@@ -67,11 +70,12 @@ PYBIND11_MODULE(PyNESO, m) {
     .def("compute_field_energy", &Diagnostics::compute_field_energy)
     .def("compute_particle_energy", &Diagnostics::compute_particle_energy);
 
-  //  m.def("initialize", &initialize);
   m.def("evolve", &evolve);
 
-  m.def("sobol_within_extents", &NESO::sobol_within_extents);  // TODO: Provide proper wrappers for extents array
+  m.def("sobol_within_extents", &NESO::sobol_within_extents);  // TODO: Provide proper wrappers for extents array; convert return-type to numpy array
   m.def("rsequence", py::overload_cast<const int, const double, const double>(&NESO::rsequence), py::arg("i"), py::arg("irrational"), py::arg("seed")=0.0);
   m.def("rsequence", py::overload_cast<const int, const int, const double>(&NESO::rsequence), py::arg("N"), py::arg("dim"), py::arg("seed")=0.0);
-  m.def("rsequence_within_extents", &NESO::rsequence_within_extents, py::arg("N"), py::arg("ndim"), py::arg("extents"), py::arg("seed")=0); // TODO: Provide proper wrappers for extents array
+  m.def("rsequence_within_extents", &NESO::rsequence_within_extents, py::arg("N"), py::arg("ndim"), py::arg("extents"), py::arg("seed")=0); // TODO: Provide proper wrappers for extents array; convert return-type to numpy array
+
+  // TODO: Write bindings for Nektar Interface?
 }
