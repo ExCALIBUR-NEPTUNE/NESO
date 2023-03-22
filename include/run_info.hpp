bb39241b659b84f63f8a06274c4399765e29982b
James Cook <james.cook@ukaea.uk>
2023-03-22 14:55:22 +0000

    ran formatting script

--- include/run_info.hpp
+++ include/run_info.hpp
@@ -9,7 +9,7 @@
 
 class RunInfo {
 public:
-  RunInfo(sycl::queue &Q,const char *git_revision, const char *git_repo_state);
+  RunInfo(sycl::queue &Q, const char *git_revision, const char *git_repo_state);
 
   std::string device_name;
   std::string device_vendor;
@@ -17,25 +17,24 @@ public:
   const char *git_revision;
   const char *git_repo_state;
 
-/*
- * Get information about execution devices
- */
+  /*
+   * Get information about execution devices
+   */
   void get_device_info(const sycl::queue &Q);
 
-/*
- * List all devices detected by SYCL
- */
+  /*
+   * List all devices detected by SYCL
+   */
   void list_all_devices();
 
-/*
- * Get information about the selected device
- */
-void get_selected_device_info(const sycl::queue &Q);
+  /*
+   * Get information about the selected device
+   */
+  void get_selected_device_info(const sycl::queue &Q);
 
-/*
- * Print run information to screen
- */
-void report_run_info();
-
+  /*
+   * Print run information to screen
+   */
+  void report_run_info();
 };
 #endif // __RUNINFO_H__
