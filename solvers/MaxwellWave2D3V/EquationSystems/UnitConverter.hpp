018fa737b406f516dc72600a8c517e39adef1704
James Cook <james.cook@ukaea.uk>
2023-06-09 15:25:27 +0100

    style unitconverter

--- solvers/MaxwellWave2D3V/EquationSystems/UnitConverter.hpp
+++ solvers/MaxwellWave2D3V/EquationSystems/UnitConverter.hpp
@@ -17,14 +17,14 @@ constexpr double EPSILON_0 = 1.0 / MU_0 / SPEED_OF_LIGHT / SPEED_OF_LIGHT;
 class UnitConverter {
   public:
 
-    UnitConverter(double lengthscale) : m_lengthscale(lengthscale) {
-      m_timescale = m_lengthscale / SPEED_OF_LIGHT;
-      m_electricpotentialscale = ELEMENTARY_MASS * std::pow(m_lengthscale / m_timescale, 2)
+    UnitConverter(double lengthScale) : m_lengthScale(lengthScale) {
+      m_timeScale = m_lengthScale / SPEED_OF_LIGHT;
+      m_electricPotentialScale = ELEMENTARY_MASS * std::pow(m_lengthScale / m_timeScale, 2)
         / ELEMENTARY_CHARGE;
-      m_chargedensityscale = m_electricpotentialscale * EPSILON_0 / std::pow(m_lengthscale, 2);
-      m_numberdensityscale = m_chargedensityscale / ELEMENTARY_CHARGE;
-      m_magneticpotentialscale = m_timescale * m_electricpotentialscale / m_lengthscale;
-      m_currentdensityscale = m_chargedensityscale * m_lengthscale / m_timescale;
+      m_chargeDensityScale = m_electricPotentialScale * EPSILON_0 / std::pow(m_lengthScale, 2);
+      m_numberDensityScale = m_chargeDensityScale / ELEMENTARY_CHARGE;
+      m_magneticPotentialScale = m_timeScale * m_electricPotentialScale / m_lengthScale;
+      m_currentDensityScale = m_chargeDensityScale * m_lengthScale / m_timeScale;
     }
 
     inline double si_numberdensity_to_sim(double n) const;
@@ -38,49 +38,49 @@ class UnitConverter {
     inline double lengthscale() const;
 
     friend std::ostream& operator<<(std::ostream &os, const UnitConverter &uc) {
-      return os << "Length scale = " << uc.m_lengthscale << "\n"
-                << "Time scale = " << uc.m_timescale << "\n"
-                << "Electric potential scale = " << uc.m_electricpotentialscale << "\n"
-                << "Charge density scale = " << uc.m_chargedensityscale << "\n"
-                << "Number density scale = " << uc.m_numberdensityscale << "\n"
-                << "Magnetic potential scale = " << uc.m_magneticpotentialscale << "\n"
-                << "Current density scale = " << uc.m_currentdensityscale << "\n"
-                << "The speed of light is = " << SPEED_OF_LIGHT / uc.m_lengthscale * uc.m_timescale;
+      return os << "Length scale = " << uc.m_lengthScale << "\n"
+                << "Time scale = " << uc.m_timeScale << "\n"
+                << "Electric potential scale = " << uc.m_electricPotentialScale << "\n"
+                << "Charge density scale = " << uc.m_chargeDensityScale << "\n"
+                << "Number density scale = " << uc.m_numberDensityScale << "\n"
+                << "Magnetic potential scale = " << uc.m_magneticPotentialScale << "\n"
+                << "Current density scale = " << uc.m_currentDensityScale << "\n"
+                << "The speed of light is = " << SPEED_OF_LIGHT / uc.m_lengthScale * uc.m_timeScale;
     }
 
   private:
-    double m_lengthscale; // multiply position in simulation by this number to get metres
-    double m_timescale; // multiple time in simulation by this number to get seconds
-    double m_electricpotentialscale; // scaling for phi as above...
-    double m_chargedensityscale; // scaling for rho
-    double m_numberdensityscale; // scaling for number density...
-    double m_magneticpotentialscale; // for A
-    double m_currentdensityscale;// for J
+    double m_lengthScale; // multiply position in simulation by this number to get metres
+    double m_timeScale; // multiple time in simulation by this number to get seconds
+    double m_electricPotentialScale; // scaling for phi as above...
+    double m_chargeDensityScale; // scaling for rho
+    double m_numberDensityScale; // scaling for number density...
+    double m_magneticPotentialScale; // for A
+    double m_currentDensityScale;// for J
 };
 
 inline double UnitConverter::si_numberdensity_to_sim(double n) const {
-  return n / m_numberdensityscale;
+  return n / m_numberDensityScale;
 }
 inline double UnitConverter::si_velocity_to_sim(double v) const {
-  return v / (m_lengthscale / m_timescale);
+  return v / (m_lengthScale / m_timeScale);
 }
 inline double UnitConverter::si_temperature_ev_to_sim(double tev) const {
   return tev * ELEMENTARY_CHARGE / (ELEMENTARY_MASS * std::pow(SPEED_OF_LIGHT, 2));
 }
 inline double UnitConverter::si_magneticfield_to_sim(double B) const {
-  return B * m_lengthscale / m_magneticpotentialscale;
+  return B * m_lengthScale / m_magneticPotentialScale;
 }
 
 inline double UnitConverter::velocity_to_si(double v) const {
-  return v * m_lengthscale / m_timescale;
+  return v * m_lengthScale / m_timeScale;
 }
 inline double UnitConverter::density_to_si(double n) const {
-  return n * m_numberdensityscale;
+  return n * m_numberDensityScale;
 }
-inline double UnitConverter::time_to_si(double t) const { return t * m_timescale; }
+inline double UnitConverter::time_to_si(double t) const { return t * m_timeScale; }
 
-inline double UnitConverter::timescale() const { return m_timescale; }
+inline double UnitConverter::timescale() const { return m_timeScale; }
 
-inline double UnitConverter::lengthscale() const { return m_lengthscale; }
+inline double UnitConverter::lengthscale() const { return m_lengthScale; }
 
 #endif
