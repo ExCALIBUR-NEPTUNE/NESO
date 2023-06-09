c76cdaf27a341a4482136e7a53285caf2789ea43
Will Saunders <will.saunders@ukaea.uk>
2023-06-09 16:52:20 +0100

    cleanup of mapping unit test

--- test/unit/nektar_interface/test_particle_mapping.cpp
+++ test/unit/nektar_interface/test_particle_mapping.cpp
@@ -27,8 +27,9 @@ static inline void copy_to_cstring(std::string input, char **output) {
   std::strcpy(*output, input.c_str());
 }
 
-// Test advecting particles between ranks
-TEST(ParticleGeometryInterface, LocalMapping2DRegular) {
+class ParticleGeometryInterface2D
+    : public testing::TestWithParam<std::tuple<std::string, double>> {};
+TEST_P(ParticleGeometryInterface2D, LocalMapping2D) {
 
   const int N_total = 2000;
   /* nektar++ maps to a scaled tolerance of 1.0-8 in Geometry2D.cpp
@@ -38,7 +39,9 @@ TEST(ParticleGeometryInterface, LocalMapping2DRegular) {
    *  where rx, ry are x,y direction residuals. This exist condition means that
    *  the absolute error in rx and ry is approximately 1.0e-4.
    */
-  const double tol = 1.0e-8;
+
+  std::tuple<std::string, double> param = GetParam();
+  const double tol = std::get<1>(param);
   int argc = 2;
   char *argv[2];
   copy_to_cstring(std::string("test_particle_geometry_interface"), &argv[0]);
@@ -47,8 +50,8 @@ TEST(ParticleGeometryInterface, LocalMapping2DRegular) {
   std::filesystem::path source_dir = source_file.parent_path();
   std::filesystem::path test_resources_dir =
       source_dir / "../../test_resources";
-  std::filesystem::path mesh_file =
-      test_resources_dir / "square_triangles_quads.xml";
+
+  std::filesystem::path mesh_file = test_resources_dir / std::get<0>(param);
 
   copy_to_cstring(std::string(mesh_file), &argv[1]);
 
@@ -164,156 +167,17 @@ TEST(ParticleGeometryInterface, LocalMapping2DRegular) {
   delete[] argv[1];
 }
 
-// Test advecting particles between ranks
-TEST(ParticleGeometryInterface, LocalMapping2DDeformed) {
-
-  const int N_total = 2000;
-  /* nektar++ maps to a scaled tolerance of 1.0-8 in Geometry2D.cpp
-   * with an exit condition like
-   *  (rx*rx + ry*ry) < tol
-   *
-   *  where rx, ry are x,y direction residuals. This exist condition means that
-   *  the absolute error in rx and ry is approximately 1.0e-4.
-   */
-  const double tol = 2.0 * 1.0e-4;
-  int argc = 2;
-  char *argv[2];
-  copy_to_cstring(std::string("test_particle_geometry_interface"), &argv[0]);
-
-  std::filesystem::path source_file = __FILE__;
-  std::filesystem::path source_dir = source_file.parent_path();
-  std::filesystem::path test_resources_dir =
-      source_dir / "../../test_resources";
-  // std::filesystem::path mesh_file =
-  //     test_resources_dir / "square_triangles_quads.xml";
-  //   TODO
-  std::filesystem::path mesh_file =
-      "/home/js0259/git-ukaea/NESO-workspace/reference_square/"
-      "reference_square_deformed_quads.xml";
-  copy_to_cstring(std::string(mesh_file), &argv[1]);
-  //   TODO
-
-  LibUtilities::SessionReaderSharedPtr session;
-  SpatialDomains::MeshGraphSharedPtr graph;
-  // Create session reader.
-  session = LibUtilities::SessionReader::CreateInstance(argc, argv);
-  graph = SpatialDomains::MeshGraph::Read(session);
-
-  auto mesh = std::make_shared<ParticleMeshInterface>(graph);
-  auto sycl_target = std::make_shared<SYCLTarget>(0, mesh->get_comm());
-
-  auto nektar_graph_local_mapper =
-      std::make_shared<NektarGraphLocalMapperT>(sycl_target, mesh, tol);
-  auto domain = std::make_shared<Domain>(mesh, nektar_graph_local_mapper);
-
-  const int ndim = 2;
-  ParticleSpec particle_spec{ParticleProp(Sym<REAL>("P"), ndim, true),
-                             ParticleProp(Sym<INT>("CELL_ID"), 1, true),
-                             ParticleProp(Sym<INT>("ID"), 1)};
-
-  auto A = std::make_shared<ParticleGroup>(domain, particle_spec, sycl_target);
-
-  NektarCartesianPeriodic pbc(sycl_target, graph, A->position_dat);
-
-  CellIDTranslation cell_id_translation(sycl_target, A->cell_id_dat, mesh);
-
-  const int rank = sycl_target->comm_pair.rank_parent;
-  const int size = sycl_target->comm_pair.size_parent;
-
-  std::mt19937 rng_pos(52234234 + rank);
-
-  int rstart, rend;
-  get_decomp_1d(size, N_total, rank, &rstart, &rend);
-  const int N = rend - rstart;
-
-  int N_check = -1;
-  MPICHK(MPI_Allreduce(&N, &N_check, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD));
-  NESOASSERT(N_check == N_total, "Error creating particles");
-
-  const int cell_count = domain->mesh->get_cell_count();
-
-  if (N > 0) {
-    auto positions =
-        uniform_within_extents(N, ndim, pbc.global_extent, rng_pos);
-
-    ParticleSet initial_distribution(N, A->get_particle_spec());
-    for (int px = 0; px < N; px++) {
-      for (int dimx = 0; dimx < ndim; dimx++) {
-        const double pos_orig = positions[dimx][px] + pbc.global_origin[dimx];
-        initial_distribution[Sym<REAL>("P")][px][dimx] = pos_orig;
-      }
-
-      initial_distribution[Sym<INT>("CELL_ID")][px][0] = 0;
-      initial_distribution[Sym<INT>("ID")][px][0] = px;
-    }
-    A->add_particles_local(initial_distribution);
-  }
-  reset_mpi_ranks((*A)[Sym<INT>("NESO_MPI_RANK")]);
-
-  MeshHierarchyGlobalMap mesh_hierarchy_global_map(
-      sycl_target, domain->mesh, A->position_dat, A->cell_id_dat,
-      A->mpi_rank_dat);
-
-  auto lambda_check_owning_cell = [&] {
-    auto point = std::make_shared<PointGeom>(ndim, -1, 0.0, 0.0, 0.0);
-    Array<OneD, NekDouble> global_coord(3);
-    Array<OneD, NekDouble> local_coord(3);
-    Array<OneD, NekDouble> local_coord_test(3);
-    for (int cellx = 0; cellx < cell_count; cellx++) {
-
-      auto positions = A->position_dat->cell_dat.get_cell(cellx);
-      auto cell_ids = A->cell_id_dat->cell_dat.get_cell(cellx);
-      auto reference_positions =
-          (*A)[Sym<REAL>("NESO_REFERENCE_POSITIONS")]->cell_dat.get_cell(cellx);
-
-      for (int rowx = 0; rowx < cell_ids->nrow; rowx++) {
-
-        const int cell_neso = (*cell_ids)[0][rowx];
-        ASSERT_EQ(cell_neso, cellx);
-        const int cell_nektar = cell_id_translation.map_to_nektar[cell_neso];
-
-        global_coord[0] = (*positions)[0][rowx];
-        global_coord[1] = (*positions)[1][rowx];
-
-        NekDouble dist;
-        auto geom = graph->GetGeometry2D(cell_nektar);
-        auto is_contained =
-            geom->ContainsPoint(global_coord, local_coord, 1.0e-10, dist);
-
-        ASSERT_TRUE(is_contained);
-
-        local_coord_test[0] = (*reference_positions)[0][rowx];
-        local_coord_test[1] = (*reference_positions)[1][rowx];
-        local_coord_test[2] = 0.0;
-
-        // check the local coordinate matches the one on the particle
-        for (int dimx = 0; dimx < ndim; dimx++) {
-          const double err =
-              ABS(local_coord[dimx] - (*reference_positions)[dimx][rowx]);
-
-          const NekDouble map_back_phys = geom->GetCoord(dimx, local_coord);
-          ASSERT_NEAR(map_back_phys, global_coord[dimx], tol);
-          const NekDouble map_back_phys_test =
-              geom->GetCoord(dimx, local_coord_test);
-          ASSERT_NEAR(map_back_phys_test, global_coord[dimx], tol);
-          ASSERT_TRUE(err <= tol);
-        }
-      }
-    }
-  };
-
-  pbc.execute();
-  mesh_hierarchy_global_map.execute();
-  A->hybrid_move();
-  cell_id_translation.execute();
-  A->cell_move();
-  lambda_check_owning_cell();
-
-  mesh->free();
-
-  delete[] argv[0];
-  delete[] argv[1];
-}
+INSTANTIATE_TEST_SUITE_P(
+    MultipleMeshes, ParticleGeometryInterface2D,
+    testing::Values(
+        std::tuple<std::string, double>("square_triangles_quads.xml", 1.0e-8),
+        std::tuple<std::string, double>(
+            "reference_squared_deformed_quads/"
+            "reference_square_deformed_quads.xml",
+            1.0e-4 // The non-linear exit tolerance in Nektar is like (err_x *
+                   // err_x
+                   // + err_y * err_y) < 1.0e-8
+            )));
 
 template <typename T, typename U, typename R>
 inline void check_geom_map(T &n, U &geom, R &rng) {
