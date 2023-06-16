4de9576351b4b2c4d5a7428b3e65f614b4850f19
Matthew Barton <wf5617@M0988-Mac.local>
2023-06-16 10:29:47 +0100

    Clang formatting and update to test case

--- include/reader_interface_classes.hpp
+++ include/reader_interface_classes.hpp
@@ -4,13 +4,14 @@
 #include <string>
 #include <vector>
 
-class ITabulatedDataReader 
-{
+class ITabulatedDataReader {
 public:
-   virtual std::vector<double> RatesData(std::string filename)=0;
-   virtual std::vector<double> GridData(std::string filename)=0;
-   virtual std::vector<std::vector<double>> ReadData(std::string filename)=0;
+  virtual std::vector<double> RatesData(std::string filename) = 0;
+  virtual std::vector<double> GridData(std::string filename) = 0;
+  virtual std::vector<std::vector<double>> ReadData(std::string filename) = 0;
+
 protected:
-   virtual std::vector<std::vector<std::string>> CSVtostring(std::string filename)=0;
+  virtual std::vector<std::vector<std::string>>
+  CSVtostring(std::string filename) = 0;
 };
 #endif
--- test/unit/test_reader.cpp
+++ test/unit/test_reader.cpp
@@ -1,14 +1,23 @@
+#include "../include/readers.hpp"
+#include <filesystem>
+#include <gtest/gtest.h>
 #include <string>
 #include <vector>
-#include "../include/readers.hpp"
 
 TEST(ReadersTest, CSV) {
 
-   TabulatedDataReaderCSV test;
-   
-   std::vector<std::vector<double>> a=test.ReadData("../test_resources/charge_exchange_h0_h1.csv");
-   ASSERT_NEAR(a[0][0],1.00,1.01)
-   ASSERT_NEAR(a[0][1],9.76e-9,9.767e-9)
-   ASSERT_NEAR(a[83][0],101361.54,101361.55)
-   ASSERT_NEAR(a[83][1],2.10e-8,2.11e-8)
-}	
+  TabulatedDataReaderCSV test;
+  std::filesystem::path source_file = __FILE__;
+  std::filesystem::path source_dir = source_file.parent_path();
+  std::filesystem::path test_resources_dir = source_dir / "../test_resources";
+  std::filesystem::path csv_test_file =
+      test_resources_dir / "charge_exchange_h0_h1.csv";
+
+  std::vector<std::vector<double>> a =
+      test.ReadData(std::string(csv_test_file));
+
+  ASSERT_NEAR(a[0][0], 1.00, 1.01);
+  ASSERT_NEAR(a[0][1], 9.76e-9, 9.767e-9);
+  ASSERT_NEAR(a[83][0], 101361.54, 101361.55);
+  ASSERT_NEAR(a[83][1], 2.10e-8, 2.11e-8);
+}
