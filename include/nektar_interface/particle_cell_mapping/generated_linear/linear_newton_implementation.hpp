c6fcfc2f9cd6a92b188feb92e457f787f593b6b7
Will Saunders <will.saunders@ukaea.uk>
2023-06-20 12:35:59 +0100

    added generated source code for linear sided 3D elements and 2D quads

---
+++ include/nektar_interface/particle_cell_mapping/generated_linear/linear_newton_implementation.hpp
@@ -0,0 +1,10 @@
+#ifndef __GENERATED_LINEAR_NEWTON_IMPLEMENTATIONS_H__
+#define __GENERATED_LINEAR_NEWTON_IMPLEMENTATIONS_H__
+
+#include "hexahedron.hpp"
+#include "prism.hpp"
+#include "pyramid.hpp"
+#include "quadrilateral.hpp"
+#include "tetrahedron.hpp"
+
+#endif
