81d804f0c62ec065922321b9666101224870926f
Matthew Barton <wf5617@M0988-Mac.local>
2023-06-21 10:20:38 +0100

    Changed for loop to make use of nrows, and updated previous tests (new tests to check error handling to be done)

--- test/unit/test_reader.cpp
+++ test/unit/test_reader.cpp
@@ -12,18 +12,17 @@ TEST(ReadersTest, CSV) {
   std::filesystem::path csv_test_file =
       test_resources_dir / "charge_exchange_h0_h1.csv";
 
-  std::vector<std::vector<double>> all_data =
-      CSVReader(std::string(csv_test_file)).GetData();
+  std::vector<std::vector<double>> all_data = CSVReader(std::string(csv_test_file)).GetData();
   std::vector<double> rates = CSVReader(std::string(csv_test_file)).GetRates();
   std::vector<double> temps = CSVReader(std::string(csv_test_file)).GetTemps();
 
-  ASSERT_NEAR(all_data[0][0], 1.00, 1.01);
-  ASSERT_NEAR(all_data[1][0], 9.76e-9, 9.767e-9);
-  ASSERT_NEAR(all_data[0][83], 101361.54, 101361.55);
-  ASSERT_NEAR(all_data[1][83], 2.10e-8, 2.11e-8);
+  ASSERT_NEAR(all_data[0][0], 1.0067508210214045, 1e-16);
+  ASSERT_NEAR(all_data[1][0], 9.762958524529584e-9, 1e-16);
+  ASSERT_NEAR(all_data[0][83], 101361.54790248517, 1e-16);
+  ASSERT_NEAR(all_data[1][83], 2.1008833642746626e-8, 1e-16);
 
-  ASSERT_NEAR(temps[0], 1.00, 1.01);
-  ASSERT_NEAR(rates[0], 9.76e-9, 9.767e-9);
-  ASSERT_NEAR(temps[83], 101361.54, 101361.55);
-  ASSERT_NEAR(rates[83], 2.10e-8, 2.11e-8);
+  ASSERT_NEAR(temps[0], 1.0067508210214045, 1e-16);
+  ASSERT_NEAR(rates[0], 9.762958524529584e-9, 1e-16);
+  ASSERT_NEAR(temps[83], 101361.54790248517, 1e-16);
+  ASSERT_NEAR(rates[83], 2.1008833642746626e-8, 1e-16);
 }
