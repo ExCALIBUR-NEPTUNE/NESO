00e5fca66df8ea70f0468861bc521484ebb163b7
Matthew Barton <wf5617@M0988-Mac.local>
2023-06-28 14:34:05 +0100

    Progress with creating 1d linear interpolator (not working)

--- test/unit/test_1D_linear_interpolator.cpp
+++ test/unit/test_1D_linear_interpolator.cpp
@@ -1,4 +1,4 @@
-#include "One_Dimensional_Linear_Interpolator.hpp"
+#include "one_dimensional_linear_interpolator.hpp"
 #include <gtest/gtest.h>
 #include <vector>
 
@@ -8,7 +8,7 @@ using namespace NESO;
 TEST(InterpolatorTest, 1DLinear) {
 	
   std::vector<double> output_test = OneDimensionalLinearInterpolator( {0,1,2,3} , {0,1,2,3} , {1.5,2.5} ).get_y();
-  std::cout<<output_test[0]<<"  "<<output_test[1]<<std::endl;
+  
   ASSERT_NEAR(output_test[0], 1.5, 1e-16);
   ASSERT_NEAR(output_test[1], 2.5, 1e-16);
 
