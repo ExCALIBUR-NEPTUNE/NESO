793abc113f4088c26f564287daed2fa7dc338fe7
Matthew Barton <wf5617@M0988-Mac.local>
2023-07-07 11:20:15 +0100

    Added in Class name into error handling

--- include/one_dimensional_linear_interpolator.hpp
+++ include/one_dimensional_linear_interpolator.hpp
@@ -47,46 +47,45 @@ protected:
     sycl::buffer<double, 1> buffer_y_output(y_output.data(),
                                             sycl::range<1>{y_output.size()});
     sycl::buffer<double, 1> buffer_dy(dy.data(), sycl::range<1>{dy.size()});
-    auto event_interpolate =
-        this->m_sycl_target->queue.submit([&](sycl::handler &cgh) {
-          auto x_data_sycl =
-              buffer_x_data.get_access<sycl::access::mode::read>(cgh);
-          auto y_data_sycl =
-              buffer_y_data.get_access<sycl::access::mode::read>(cgh);
-          auto x_input_sycl =
-              buffer_x_input.get_access<sycl::access::mode::read>(cgh);
-          auto y_output_sycl =
-              buffer_y_output.get_access<sycl::access::mode::read_write>(cgh);
-          auto dy_sycl = buffer_dy.get_access<sycl::access::mode::read>(cgh);
-          cgh.parallel_for<>(
-              sycl::range<1>(y_output_sycl.size()), [=](sycl::id<1> idx) {
-                int k;
-                for (int i = 0; i < x_data_sycl.size(); i++) {
-                  // error handling
-                  if (x_input_sycl[int(idx)] < x_data_sycl[0] or
-                      x_input_sycl[int(idx)] >
-                          x_data_sycl[x_data_sycl.size() - 1]) {
-                    std::cerr
-                        << "Input value out of " << x_input_sycl[int(idx)]
-                        << " outside of range  of x values provided by data"
-                        << std::endl;
-                    // throw an error
-                    NESO_KERNEL_ASSERT(false, k_ep);
-                    break;
-                  }
-                  // set value of index to first value for which x value
-                  // of input is greater than x data values
-                  if (x_input_sycl[int(idx)] - x_data_sycl[i] < 0.0) {
-                    k = i;
-                    break;
-                  }
-                }
-                y_output_sycl[int(idx)] =
-                    y_data_sycl[k - 1] +
-                    dy_sycl[k] * (x_input_sycl[int(idx)] - x_data_sycl[k - 1]) /
-                        (x_data_sycl[k] - x_data_sycl[k - 1]);
-              });
-        });
+    auto event_interpolate = this->m_sycl_target->queue.submit([&](sycl::handler
+                                                                       &cgh) {
+      auto x_data_sycl =
+          buffer_x_data.get_access<sycl::access::mode::read>(cgh);
+      auto y_data_sycl =
+          buffer_y_data.get_access<sycl::access::mode::read>(cgh);
+      auto x_input_sycl =
+          buffer_x_input.get_access<sycl::access::mode::read>(cgh);
+      auto y_output_sycl =
+          buffer_y_output.get_access<sycl::access::mode::read_write>(cgh);
+      auto dy_sycl = buffer_dy.get_access<sycl::access::mode::read>(cgh);
+      cgh.parallel_for<>(sycl::range<1>(y_output_sycl.size()), [=](sycl::id<1>
+                                                                       idx) {
+        int k;
+        for (int i = 0; i < x_data_sycl.size(); i++) {
+          // error handling
+          if (x_input_sycl[int(idx)] < x_data_sycl[0] or
+              x_input_sycl[int(idx)] > x_data_sycl[x_data_sycl.size() - 1]) {
+            std::cerr << "OneDimensionalLinearInterpolator: Input value out of "
+                      << x_input_sycl[int(idx)]
+                      << " outside of range  of x values provided by data"
+                      << std::endl;
+            // throw an error
+            NESO_KERNEL_ASSERT(false, k_ep);
+            break;
+          }
+          // set value of index to first value for which x value
+          // of input is greater than x data values
+          if (x_input_sycl[int(idx)] - x_data_sycl[i] < 0.0) {
+            k = i;
+            break;
+          }
+        }
+        y_output_sycl[int(idx)] =
+            y_data_sycl[k - 1] +
+            dy_sycl[k] * (x_input_sycl[int(idx)] - x_data_sycl[k - 1]) /
+                (x_data_sycl[k] - x_data_sycl[k - 1]);
+      });
+    });
     event_interpolate.wait_and_throw();
     error = ep.get_flag();
   }
