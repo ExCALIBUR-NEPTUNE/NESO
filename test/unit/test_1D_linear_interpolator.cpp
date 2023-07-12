4c9a25c09dd3de4202d5a37e825ac49bcce6c37e
Matthew Barton <wf5617@M0988-Mac.local>
2023-07-12 09:36:48 +0100

    Updated test so gradients change to test indexing in interpolator (tested working)

--- test/unit/test_1D_linear_interpolator.cpp
+++ test/unit/test_1D_linear_interpolator.cpp
@@ -14,8 +14,8 @@ TEST(InterpolatorTest, 1DLinear) {
   std::vector<double> &test_input_ref = test_input;
   std::vector<double> test_output(test_input.size());
   std::vector<double> &test_output_ref = test_output;
-  LinearInterpolator1D({0, 1, 2, 3}, {0, 1, 2, 3}, sycl_target)
+  LinearInterpolator1D({0, 1, 2, 3}, {0, 1, 3, 4}, sycl_target)
       .interpolate(test_input_ref, test_output_ref);
-  ASSERT_NEAR(test_output[0], 1.5, 1e-16);
-  ASSERT_NEAR(test_output[1], 2.5, 1e-16);
+  ASSERT_NEAR(test_output[0], 2.0, 1e-16);
+  ASSERT_NEAR(test_output[1], 3.5, 1e-16);
 }
