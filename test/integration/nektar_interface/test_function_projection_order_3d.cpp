fca0db9cdb33b4d6e87c218015e6d106ea82e5c8
Will Saunders <will.saunders@ukaea.uk>
2023-07-25 16:46:27 +0100

    Duplicate test/test_resources/reference_prism_tet_cube/conditions_nummodes_2.xml history in test/test_resources/reference_prism_tet_cube/conditions_cg_nummodes_2.xml history.

---
+++ test/integration/nektar_interface/test_function_projection_order_3d.cpp
@@ -0,0 +1,33 @@
+#include "nektar_interface/function_projection.hpp"
+#include "nektar_interface/particle_interface.hpp"
+#include "nektar_interface/utilities.hpp"
+#include <LibUtilities/BasicUtils/SessionReader.h>
+#include <MultiRegions/DisContField.h>
+#include <SolverUtils/Driver.h>
+#include <array>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <filesystem>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <random>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace Nektar;
+using namespace Nektar::SolverUtils;
+using namespace Nektar::SpatialDomains;
+using namespace Nektar::MultiRegions;
+using namespace NESO::Particles;
+
+static inline void copy_to_cstring(std::string input, char **output) {
+  *output = new char[input.length() + 1];
+  std::strcpy(*output, input.c_str());
+}
+
+
