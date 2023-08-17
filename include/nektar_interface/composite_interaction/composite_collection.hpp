b43eddd11531c96801c1113a0d13d706b2de1867
Will Saunders <will.saunders@ukaea.uk>
2023-08-17 17:07:09 +0100

    Implementation to populate a map (BlockedBinaryTree) from cells to geometry objects and LinePlaneIntersection objects

---
+++ include/nektar_interface/composite_interaction/composite_collection.hpp
@@ -0,0 +1,39 @@
+#ifndef __COMPOSITE_COLLECTION_H_
+#define __COMPOSITE_COLLECTION_H_
+
+#include <SpatialDomains/MeshGraph.h>
+using namespace Nektar;
+
+#include <neso_particles.hpp>
+using namespace NESO::Particles;
+
+#include <nektar_interface/geometry_transport/packed_geom_2d.hpp>
+#include <nektar_interface/particle_mesh_interface.hpp>
+
+#include "line_plane_intersection.hpp"
+
+#include <map>
+#include <memory>
+#include <set>
+#include <utility>
+#include <vector>
+
+namespace NESO::CompositeInteraction {
+
+/**
+ * Struct pointed to by a BlockedBinaryTree for each MeshHierarchy cell.
+ */
+struct CompositeCollection {
+  int num_quads;
+  int num_tris;
+  LinePlaneIntersection *lpi_quads;
+  LinePlaneIntersection *lpi_tris;
+  size_t stride_quads;
+  size_t stride_tris;
+  unsigned char *buf_quads;
+  unsigned char *buf_tris;
+};
+
+} // namespace NESO::CompositeInteraction
+
+#endif
--- include/nektar_interface/composite_interaction/composite_collections.hpp
+++ include/nektar_interface/composite_interaction/composite_collections.hpp
@@ -7,8 +7,10 @@ using namespace Nektar;
 #include <neso_particles.hpp>
 using namespace NESO::Particles;
 
+#include "composite_collection.hpp"
 #include "composite_transport.hpp"
 #include <nektar_interface/geometry_transport/packed_geom_2d.hpp>
+#include <nektar_interface/particle_cell_mapping/newton_geom_interfaces.hpp>
 #include <nektar_interface/particle_mesh_interface.hpp>
 
 #include <map>
@@ -26,6 +28,16 @@ class CompositeCollections {
 protected:
   ParticleMeshInterfaceSharedPtr particle_mesh_interface;
 
+  // Stack for device buffers (LinePlaneIntersections and Newton X mapping data)
+  std::stack<std::shared_ptr<BufferDevice<unsigned char>>> stack_geometry_data;
+  // Stack for the device buffers (CompositeCollection)
+  std::stack<std::shared_ptr<BufferDevice<CompositeCollection>>>
+      stack_collection_data;
+
+  /**
+   *  This method takes the packed geoms and creates the node the in blocked
+   *  binary tree such that the kernel can access the geometry info.
+   */
   inline void collect_cell(const INT cell) {
 
     std::vector<std::shared_ptr<RemoteGeom2D<SpatialDomains::QuadGeom>>>
@@ -34,15 +46,85 @@ protected:
         remote_tris;
 
     this->composite_transport->get_geometry(cell, remote_quads, remote_tris);
-    nprint("cell:", cell, "qsize:", remote_quads.size(),
-           "tsize:", remote_tris.size());
 
-    for (auto gx : remote_quads) {
-      nprint("\t quad:", gx->rank, gx->id);
+    const int num_quads = remote_quads.size();
+    const int num_tris = remote_tris.size();
+
+    Newton::MappingQuadLinear2DEmbed3D mapper_quads{};
+    Newton::MappingTriangleLinear2DEmbed3D mapper_tris{};
+
+    const int stride_quads = mapper_quads.data_size_device();
+    const int stride_tris = mapper_tris.data_size_device();
+    NESOASSERT(mapper_quads.data_size_host() == 0,
+               "Expected 0 host bytes required for this mapper.");
+    NESOASSERT(mapper_tris.data_size_host() == 0,
+               "Expected 0 host bytes required for this mapper.");
+
+    // we pack all the device data for the MeshHierachy cell into a single
+    // device buffer
+    const int stride_lpi = static_cast<int>(sizeof(LinePlaneIntersection));
+    const int num_bytes = stride_quads * num_quads + stride_tris * num_tris +
+                          stride_lpi * num_quads + stride_lpi * num_tris;
+    const int offset_quads = 0;
+    const int offset_tris = offset_quads + stride_quads * num_quads;
+    const int offset_lpi_quads = offset_tris + stride_tris * num_tris;
+    const int offset_lpi_tris = offset_lpi_quads + stride_lpi * num_quads;
+
+    std::vector<unsigned char> buf(num_bytes);
+
+    LinePlaneIntersection *lpi_quads = static_cast<LinePlaneIntersection *>(
+        static_cast<void *>(buf.data() + offset_lpi_quads));
+    LinePlaneIntersection *lpi_tris = static_cast<LinePlaneIntersection *>(
+        static_cast<void *>(buf.data() + offset_lpi_tris));
+
+    unsigned char *map_data_quads = buf.data() + offset_quads;
+    unsigned char *map_data_tris = buf.data() + offset_tris;
+
+    for (int gx = 0; gx < num_quads; gx++) {
+      auto remote_geom = remote_quads[gx];
+      auto geom = remote_geom->geom;
+      mapper_quads.write_data(geom, nullptr,
+                              map_data_quads + gx * stride_quads);
+      LinePlaneIntersection lpi(geom);
+      lpi_quads[gx] = lpi;
     }
-    for (auto gx : remote_tris) {
-      nprint("\t tri:", gx->rank, gx->id);
+
+    for (int gx = 0; gx < num_tris; gx++) {
+      auto remote_geom = remote_tris[gx];
+      auto geom = remote_geom->geom;
+      mapper_tris.write_data(geom, nullptr, map_data_tris + gx * stride_tris);
+      LinePlaneIntersection lpi(geom);
+      lpi_tris[gx] = lpi;
     }
+
+    // create a device buffer from the vector
+    auto d_buf =
+        std::make_shared<BufferDevice<unsigned char>>(this->sycl_target, buf);
+    this->stack_geometry_data.push(d_buf);
+    unsigned char *d_ptr = d_buf->ptr;
+
+    // create the CompositeCollection collection object that points to the
+    // geometry data we just placed on the device
+    std::vector<CompositeCollection> cc(1);
+    cc[0].num_quads = num_quads;
+    cc[0].num_tris = num_tris;
+    cc[0].lpi_quads = static_cast<LinePlaneIntersection *>(
+        static_cast<void *>(d_ptr + offset_lpi_quads));
+    cc[0].lpi_tris = static_cast<LinePlaneIntersection *>(
+        static_cast<void *>(d_ptr + offset_lpi_tris));
+    cc[0].stride_quads = stride_quads;
+    cc[0].stride_tris = stride_tris;
+    cc[0].buf_quads = d_ptr + offset_quads;
+    cc[0].buf_tris = d_ptr + offset_tris;
+
+    // create the device buffer that holds this CompositeCollection
+    auto d_cc_buf = std::make_shared<BufferDevice<CompositeCollection>>(
+        this->sycl_target, cc);
+    this->stack_collection_data.push(d_cc_buf);
+
+    // add the device pointer to the CompositeCollection we just created into
+    // the BlockedBinaryTree
+    this->map_cells_collections->add(cell, d_cc_buf->ptr);
   }
 
 public:
@@ -56,6 +138,11 @@ public:
 
   /// The composite transport instance.
   std::unique_ptr<CompositeTransport> composite_transport;
+
+  /// The container that holds the map from MeshHierarchy cells to the geometry
+  /// objects for the composites in those cells.
+  std::shared_ptr<BlockedBinaryTree<INT, CompositeCollection *, 4>>
+      map_cells_collections;
 
   /**
    * TODO
@@ -73,6 +160,10 @@ public:
 
     this->composite_transport = std::make_unique<CompositeTransport>(
         particle_mesh_interface, composite_indices);
+
+    this->map_cells_collections =
+        std::make_shared<BlockedBinaryTree<INT, CompositeCollection *, 4>>(
+            this->sycl_target);
 
     for (auto cx : this->composite_transport->held_cells) {
       this->collect_cell(cx);
