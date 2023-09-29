b28bf19a3e34c79620995b05b3d8d8c5e9e1281c
Will Saunders <will.saunders@ukaea.uk>
2023-09-29 18:51:10 +0100

    proof of concept explicit vectors

--- test/unit/nektar_interface/test_particle_function_evaluation.cpp
+++ test/unit/nektar_interface/test_particle_function_evaluation.cpp
@@ -604,3 +604,203 @@ TEST(ParticleFunctionEvaluation, ContFieldDerivative) {
   delete[] argv[1];
   delete[] argv[2];
 }
+
+
+
+
+
+
+
+
+
+
+
+
+TEST(Foo, Bar) {
+
+  const int N_total = 2000000;
+  const double tol = 1.0e-10;
+  int argc = 3;
+  char *argv[3];
+
+  /*
+  std::filesystem::path source_file = __FILE__;
+  std::filesystem::path source_dir = source_file.parent_path();
+  std::filesystem::path test_resources_dir =
+      source_dir / "../../test_resources";
+  std::filesystem::path mesh_file =
+      test_resources_dir / "square_triangles_quads_nummodes_6.xml";
+  std::filesystem::path conditions_file = test_resources_dir / "conditions.xml";
+  */
+
+  std::filesystem::path mesh_file = "/home/js0259/git-ukaea/NESO-workspace/reference_square/reference_square.xml";
+  std::filesystem::path conditions_file = "/home/js0259/git-ukaea/NESO-workspace/reference_square/conditions.xml";
+
+  copy_to_cstring(std::string("test_particle_function_evaluation"), &argv[0]);
+  copy_to_cstring(std::string(mesh_file), &argv[1]);
+  copy_to_cstring(std::string(conditions_file), &argv[2]);
+
+  LibUtilities::SessionReaderSharedPtr session;
+  SpatialDomains::MeshGraphSharedPtr graph;
+  // Create session reader.
+  session = LibUtilities::SessionReader::CreateInstance(argc, argv);
+  graph = SpatialDomains::MeshGraph::Read(session);
+
+  auto dis_cont_field = std::make_shared<DisContField>(session, graph, "u");
+
+  auto mesh = std::make_shared<ParticleMeshInterface>(graph);
+  auto sycl_target = std::make_shared<SYCLTarget>(0, mesh->get_comm());
+
+  auto nektar_graph_local_mapper =
+      std::make_shared<NektarGraphLocalMapper>(sycl_target, mesh);
+
+  auto domain = std::make_shared<Domain>(mesh, nektar_graph_local_mapper);
+
+  const int ndim = 2;
+  ParticleSpec particle_spec{ParticleProp(Sym<REAL>("P"), ndim, true),
+                             ParticleProp(Sym<INT>("CELL_ID"), 1, true),
+                             ParticleProp(Sym<REAL>("FUNC_EVALS"), 2)};
+
+  auto A = std::make_shared<ParticleGroup>(domain, particle_spec, sycl_target);
+
+  NektarCartesianPeriodic pbc(sycl_target, graph, A->position_dat);
+  auto cell_id_translation =
+      std::make_shared<CellIDTranslation>(sycl_target, A->cell_id_dat, mesh);
+
+  const int rank = sycl_target->comm_pair.rank_parent;
+  const int size = sycl_target->comm_pair.size_parent;
+
+  std::mt19937 rng_pos(52234234 + rank);
+
+  int rstart, rend;
+  get_decomp_1d(size, N_total, rank, &rstart, &rend);
+  const int N = rend - rstart;
+
+  int N_check = -1;
+  MPICHK(MPI_Allreduce(&N, &N_check, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD));
+  NESOASSERT(N_check == N_total, "Error creating particles");
+
+  const int cell_count = domain->mesh->get_cell_count();
+
+  if (N > 0) {
+    auto positions =
+        uniform_within_extents(N, ndim, pbc.global_extent, rng_pos);
+
+    std::uniform_int_distribution<int> uniform_dist(
+        0, sycl_target->comm_pair.size_parent - 1);
+    ParticleSet initial_distribution(N, A->get_particle_spec());
+    for (int px = 0; px < N; px++) {
+      for (int dimx = 0; dimx < ndim; dimx++) {
+        const double pos_orig = positions[dimx][px] + pbc.global_origin[dimx];
+        initial_distribution[Sym<REAL>("P")][px][dimx] = pos_orig;
+      }
+      initial_distribution[Sym<INT>("CELL_ID")][px][0] = px % cell_count;
+    }
+    A->add_particles_local(initial_distribution);
+  }
+  reset_mpi_ranks((*A)[Sym<INT>("NESO_MPI_RANK")]);
+
+  MeshHierarchyGlobalMap mesh_hierarchy_global_map(
+      sycl_target, domain->mesh, A->position_dat, A->cell_id_dat,
+      A->mpi_rank_dat);
+
+  pbc.execute();
+  mesh_hierarchy_global_map.execute();
+  A->hybrid_move();
+  cell_id_translation->execute();
+  A->cell_move();
+
+  auto lambda_f = [&](const NekDouble x, const NekDouble y) {
+    return 2.0 * (x + 0.5) * (x - 0.5) * (y + 0.8) * (y - 0.8);
+  };
+
+  interpolate_onto_nektar_field_2d(lambda_f, dis_cont_field);
+
+  // write_vtu(dis_cont_field, "func.vtu");
+
+  // create evaluation object
+  auto field_evaluate = std::make_shared<FunctionEvaluateBasis<DisContField>>(
+      dis_cont_field, mesh, cell_id_translation);
+
+  // evaluate field at particle locations
+
+  const auto global_coeffs = dis_cont_field->GetCoeffs();
+  field_evaluate->evaluate_test_init(A, Sym<REAL>("FUNC_EVALS"), 0,  global_coeffs);
+  field_evaluate->evaluate_test(A, Sym<REAL>("FUNC_EVALS"), 0,  global_coeffs);
+
+  // check evaluations
+  for (int cellx = 0; cellx < cell_count; cellx++) {
+
+    auto positions = A->position_dat->cell_dat.get_cell(cellx);
+    auto func_evals = (*A)[Sym<REAL>("FUNC_EVALS")]->cell_dat.get_cell(cellx);
+
+    for (int rowx = 0; rowx < positions->nrow; rowx++) {
+
+      const double x = (*positions)[0][rowx];
+      const double y = (*positions)[1][rowx];
+
+      const double eval_dat = (*func_evals)[0][rowx];
+      // not expected to match due to BCs
+      const double eval_correct = evaluate_scalar_2d(dis_cont_field, x, y);
+      const double err = ABS(eval_correct - eval_dat);
+
+      EXPECT_NEAR(eval_correct, eval_dat, 1.0e-5);
+    }
+  }
+
+  const int N_TEST = 10;
+ 
+  {
+  auto t0 = profile_timestamp();
+  MPI_Barrier(MPI_COMM_WORLD);
+  for(int tx=0 ; tx<N_TEST ; tx++){
+    field_evaluate->evaluate(A, Sym<REAL>("FUNC_EVALS"), 0,  global_coeffs);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  auto t1 = profile_timestamp();
+  auto tt = profile_elapsed(t0, t1);
+  if (rank == 0){
+    nprint("EXISTING:", tt / N_TEST);
+  }
+  }
+  {
+
+  auto t0 = profile_timestamp();
+  MPI_Barrier(MPI_COMM_WORLD);
+  for(int tx=0 ; tx<N_TEST ; tx++){
+    field_evaluate->evaluate_test_init(A, Sym<REAL>("FUNC_EVALS"), 0,  global_coeffs);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  auto t1 = profile_timestamp();
+  auto tt = profile_elapsed(t0, t1);
+  if (rank == 0){
+    nprint("INIT    :", tt / N_TEST);
+  }
+  }
+
+  {
+
+  field_evaluate->evaluate_test_init(A, Sym<REAL>("FUNC_EVALS"), 0,  global_coeffs);
+
+  auto t0 = profile_timestamp();
+  MPI_Barrier(MPI_COMM_WORLD);
+  for(int tx=0 ; tx<N_TEST ; tx++){
+    field_evaluate->evaluate_test(A, Sym<REAL>("FUNC_EVALS"), 0,  global_coeffs);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  auto t1 = profile_timestamp();
+  auto tt = profile_elapsed(t0, t1);
+  if (rank == 0){
+    nprint("NEW     :", tt / N_TEST);
+  }
+  }
+
+
+  A->free();
+  sycl_target->free();
+  mesh->free();
+
+  delete[] argv[0];
+  delete[] argv[1];
+  delete[] argv[2];
+}
