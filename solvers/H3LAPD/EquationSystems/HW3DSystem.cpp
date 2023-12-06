5eccd35becd904dcb3b4090cb98514613a657f10
Owen Parry <owen.parry@ukaea.uk>
2023-12-06 11:25:06 +0000

    Working 3D implementation and example.

--- solvers/H3LAPD/EquationSystems/HW3DSystem.cpp
+++ solvers/H3LAPD/EquationSystems/HW3DSystem.cpp
@@ -14,7 +14,8 @@ std::string HW3DSystem::class_name =
 HW3DSystem::HW3DSystem(const LU::SessionReaderSharedPtr &session,
                        const SD::MeshGraphSharedPtr &graph)
     : UnsteadySystem(session, graph), AdvectionSystem(session, graph),
-      DriftReducedSystem(session, graph) {
+      DriftReducedSystem(session, graph), m_diff_in_arr(1), m_diff_out_arr(1),
+      m_diff_fields(0) {
   m_required_flds = {"ne", "w", "phi"};
   m_int_fld_names = {"ne", "w"};
 
@@ -44,6 +45,31 @@ void HW3DSystem::calc_E_and_adv_vels(
     Vmath::Svtvp(npts, m_b_unit[iDim], m_par_vel_elec, 1, m_ExB_vel[iDim], 1,
                  m_adv_vel_elec[iDim], 1);
   }
+}
+
+/**
+ *@brief Calculate parallel dynamics term
+ *
+ * @param in_arr physical values of all fields
+ */
+void HW3DSystem::calc_par_dyn_term(
+    const Array<OneD, const Array<OneD, NekDouble>> &in_arr) {
+
+  int npts = GetNpoints();
+  int ne_idx = m_field_to_index.get_idx("ne");
+  int phi_idx = m_field_to_index.get_idx("phi");
+
+  // Zero temporary out array
+  zero_out_array(m_diff_out_arr);
+
+  // Write phi-n into temporary input array
+  Vmath::Vsub(npts, m_fields[phi_idx]->GetPhys(), 1, in_arr[ne_idx], 1,
+              m_diff_in_arr[0], 1);
+  // Use diffusion object to calculate second deriv of phi-n in z direction
+  m_diffusion->Diffuse(1, m_diff_fields, m_diff_in_arr, m_diff_out_arr);
+  // Multiply by constants to compute term
+  Vmath::Smul(npts, m_omega_ce / m_nu_ei, m_diff_out_arr[0], 1, m_par_dyn_term,
+              1);
 }
 
 /**
@@ -85,15 +111,42 @@ void HW3DSystem::explicit_time_int(
   add_adv_terms({"ne"}, m_adv_elec, m_adv_vel_elec, in_arr, out_arr, time);
   add_adv_terms({"w"}, m_adv_vort, m_ExB_vel, in_arr, out_arr, time);
 
+  // Add parallel dynamics, implemented as anisotropic parallel-only diffusion
+  calc_par_dyn_term(in_arr);
+  Vmath::Vsub(npts, out_arr[ne_idx], 1, m_par_dyn_term, 1, out_arr[ne_idx], 1);
+  Vmath::Vsub(npts, out_arr[w_idx], 1, m_par_dyn_term, 1, out_arr[w_idx], 1);
+
   // Add \kappa*\dpartial\phi/\dpartial y to RHS
-  Array<OneD, NekDouble> HWterm_2D_kappa(npts);
-  m_fields[phi_idx]->PhysDeriv(1, m_fields[phi_idx]->GetPhys(),
-                               HWterm_2D_kappa);
-  Vmath::Smul(npts, m_kappa, HWterm_2D_kappa, 1, HWterm_2D_kappa, 1);
-  Vmath::Vsub(npts, out_arr[ne_idx], 1, HWterm_2D_kappa, 1, out_arr[ne_idx], 1);
+  Array<OneD, NekDouble> kappa_term(npts);
+  m_fields[phi_idx]->PhysDeriv(1, m_fields[phi_idx]->GetPhys(), kappa_term);
+  Vmath::Smul(npts, m_kappa, kappa_term, 1, kappa_term, 1);
+  Vmath::Vsub(npts, out_arr[ne_idx], 1, kappa_term, 1, out_arr[ne_idx], 1);
 
   // Add particle sources
   add_particle_sources({"ne"}, out_arr);
+}
+
+/**
+ * @brief Return the flux vector for the unsteady diffusion problem.
+ */
+void HW3DSystem::get_flux_vector_diff(
+    const Array<OneD, Array<OneD, NekDouble>> &in_arr,
+    const Array<OneD, Array<OneD, Array<OneD, NekDouble>>> &q_field,
+    Array<OneD, Array<OneD, Array<OneD, NekDouble>>> &viscous_tensor) {
+  boost::ignore_unused(in_arr);
+
+  unsigned int nDim = q_field.size();
+  unsigned int nConvectiveFields = q_field[0].size();
+  unsigned int nPts = q_field[0][0].size();
+
+  // Hard-code coefficients - assumes field in z direction
+  NekDouble d[3] = {0.0, 0.0, 1.0};
+
+  for (unsigned int j = 0; j < nDim; ++j) {
+    for (unsigned int i = 0; i < nConvectiveFields; ++i) {
+      Vmath::Smul(nPts, d[j], q_field[j][i], 1, viscous_tensor[j][i], 1);
+    }
+  }
 }
 
 /**
@@ -115,6 +168,8 @@ void HW3DSystem::get_phi_solve_rhs(
  */
 void HW3DSystem::load_params() {
   DriftReducedSystem::load_params();
+  // Diffusion type
+  m_session->LoadSolverInfo("DiffusionType", m_diff_type, "LDG");
 
   // kappa (required)
   m_session->LoadParameter("HW_kappa", m_kappa);
@@ -135,13 +190,25 @@ void HW3DSystem::v_InitObject(bool DeclareField) {
   // Bind RHS function for time integration object
   m_ode.DefineOdeRhs(&HW3DSystem::explicit_time_int, this);
 
-  // Create diagnostic for recording growth rates
-  if (m_diag_growth_rates_recording_enabled) {
-    m_diag_growth_rates_recorder =
-        std::make_shared<GrowthRatesRecorder<MultiRegions::DisContField>>(
-            m_session, m_discont_fields["ne"], m_discont_fields["w"],
-            m_discont_fields["phi"], GetNpoints(), m_omega_ce, m_kappa);
-  }
+  // Set up diffusion object
+  m_diffusion =
+      SU::GetDiffusionFactory().CreateInstance(m_diff_type, m_diff_type);
+  m_diffusion->SetFluxVector(&HW3DSystem::get_flux_vector_diff, this);
+  m_diffusion->InitObject(m_session, m_fields);
+
+  // Allocate temporary arrays used in the diffusion calc
+  int npts = GetNpoints();
+  m_diff_in_arr[0] = Array<OneD, NekDouble>(npts);
+  m_diff_out_arr[0] = Array<OneD, NekDouble>(npts, 0.0);
+  m_diff_fields[0] = m_fields[m_field_to_index.get_idx("ne")];
+
+  // // Create diagnostic for recording growth rates
+  // if (m_diag_growth_rates_recording_enabled) {
+  //   m_diag_growth_rates_recorder =
+  //       std::make_shared<GrowthRatesRecorder<MultiRegions::DisContField>>(
+  //           m_session, m_discont_fields["ne"], m_discont_fields["w"],
+  //           m_discont_fields["phi"], GetNpoints(), m_alpha, m_kappa);
+  // }
 
   // Create diagnostic for recording fluid and particles masses
   if (m_diag_mass_recording_enabled) {
--- solvers/H3LAPD/EquationSystems/HW3DSystem.hpp
+++ solvers/H3LAPD/EquationSystems/HW3DSystem.hpp
@@ -5,6 +5,7 @@
 
 #include <LibUtilities/Memory/NekMemoryManager.hpp>
 #include <SolverUtils/AdvectionSystem.h>
+#include <SolverUtils/Diffusion/Diffusion.h>
 #include <SolverUtils/EquationSystem.h>
 #include <SolverUtils/Forcing/Forcing.h>
 #include <SolverUtils/RiemannSolvers/RiemannSolver.h>
@@ -56,6 +57,9 @@ protected:
   HW3DSystem(const LU::SessionReaderSharedPtr &session,
              const SD::MeshGraphSharedPtr &graph);
 
+  void
+  calc_par_dyn_term(const Array<OneD, const Array<OneD, NekDouble>> &in_arr);
+
   virtual void calc_E_and_adv_vels(
       const Array<OneD, const Array<OneD, NekDouble>> &inarray) override;
 
@@ -63,6 +67,11 @@ protected:
   explicit_time_int(const Array<OneD, const Array<OneD, NekDouble>> &inarray,
                     Array<OneD, Array<OneD, NekDouble>> &outarray,
                     const NekDouble time) override;
+
+  void get_flux_vector_diff(
+      const Array<OneD, Array<OneD, NekDouble>> &in_arr,
+      const Array<OneD, Array<OneD, Array<OneD, NekDouble>>> &q_field,
+      Array<OneD, Array<OneD, Array<OneD, NekDouble>>> &viscous_tensor);
 
   void
   get_phi_solve_rhs(const Array<OneD, const Array<OneD, NekDouble>> &inarray,
@@ -80,6 +89,13 @@ private:
   bool m_diag_growth_rates_recording_enabled;
   /// Bool to enable/disable mass recordings
   bool m_diag_mass_recording_enabled;
+
+  // Diffussion type
+  std::string m_diff_type;
+
+  // Diffusion object
+  SU::DiffusionSharedPtr m_diffusion;
+
   /**
    * Hasegawa-Wakatani κ - not clear whether this should be included in 3D or
    * not
@@ -89,6 +105,13 @@ private:
   NekDouble m_nu_ei;
   /// Cyclotron frequency for electrons
   NekDouble m_omega_ce;
+
+  // Array for storage of parallel dynamics term
+  Array<OneD, NekDouble> m_par_dyn_term;
+
+  // Arrays to store temporary fields and values for the diffusion operation
+  Array<OneD, MultiRegions::ExpListSharedPtr> m_diff_fields;
+  Array<OneD, Array<OneD, NekDouble>> m_diff_in_arr, m_diff_out_arr;
 };
 
 } // namespace NESO::Solvers::H3LAPD
