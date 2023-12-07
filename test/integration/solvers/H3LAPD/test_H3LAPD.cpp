1cd6318c92dc666f4cbb843bcdc0d6f95087214c
Owen Parry <owen.parry@ukaea.uk>
2023-12-07 14:54:56 +0000

    Add test for energy, enstrophy growth rates in 3D. Energy slightly off for some reason.

--- test/integration/solvers/H3LAPD/test_H3LAPD.cpp
+++ test/integration/solvers/H3LAPD/test_H3LAPD.cpp
@@ -14,4 +14,4 @@ TEST_F(HWTest, 2Din3DHWGrowthRates) { check_growth_rates(); }
 
 TEST_F(HWTest, Coupled2Din3DHWMassCons) { check_mass_cons(); }
 
-// TEST_F(HWTest, 3DHWGrowthRates) { check_growth_rates(); }
+TEST_F(HWTest, 3DHWGrowthRates) { check_growth_rates(); }
