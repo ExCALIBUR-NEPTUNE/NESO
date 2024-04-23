a4fadf358048b7cedfb2ceaf0e7aca0154fa586c
Will Saunders <will.saunders@ukaea.uk>
2024-04-23 14:12:27 +0100

    made formatting script more robust to config files not existing

--- scripts/format_all.sh
+++ scripts/format_all.sh
@@ -1,8 +1,21 @@
-# clang-format
-find ./src ./include ./test ./solvers -iname \*.hpp -o -iname \*.cpp | xargs clang-format -i
-# cmake-format
-cmake-format -i CMakeLists.txt # Do this one on it's own so we don't go into build etc
-find ./src ./include ./test ./solvers -iname CMakeLists.txt | xargs cmake-format -i
-# black
-find ./python -iname \*.py | xargs black
+# This script formats the source files in NESO. This script should be ran from
+# the root of the git repository. This script should be run as:
+#
+#   bash format_all.sh
+#
+# to format all source files. 
+
+if [[ -f .clang-format && -f .cmake-format ]]; then
+    # clang-format
+    find ./src ./include ./test ./solvers -iname \*.hpp -o -iname \*.cpp | xargs clang-format -i
+    # cmake-format
+    cmake-format -c .cmake-format -i CMakeLists.txt # Do this one on it's own so we don't go into build etc
+    find ./src ./include ./test ./solvers -iname CMakeLists.txt | xargs cmake-format  -c .cmake-format -i
+    # black
+    find ./python -iname \*.py | xargs black
+else
+    echo "ERROR: The files .clang-format and .cmake-format do not exist. Please
+       check this script is executed from the root directory of the git
+       repository."
+fi
 
