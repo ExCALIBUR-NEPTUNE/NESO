e7e274ed0cd41c922c5a8cca1a309ed338ab776f
Owen Parry <owen.parry@ukaea.uk>
2024-06-24 16:28:38 +0100

    Move PartSysBase implementation into a separate file.

--- include/nektar_interface/solver_base/partsys_base.hpp
+++ include/nektar_interface/solver_base/partsys_base.hpp
@@ -37,10 +37,10 @@ public:
   SYCLTargetSharedPtr sycl_target;
 
   /// @brief Report particle parameter values (writes to stdout).
-  inline void add_params_report();
+  void add_params_report();
 
   /// @brief Clear up memory related to the particle system
-  inline void free();
+  void free();
 
   /**
    * @brief Return true if \p step is a scheduled output step, according to the
@@ -48,13 +48,13 @@ public:
    *
    * @ param step
    */
-  inline bool is_output_step(int step);
+  bool is_output_step(int step);
 
   /**
    *  @brief Write particle properties to an output file.
    *  @param step Time step number.
    */
-  inline void write(const int step);
+  void write(const int step);
 
 protected:
   /**
@@ -65,10 +65,10 @@ protected:
    *  @param comm (optional) MPI communicator to use - default MPI_COMM_WORLD.
    *
    */
-  inline PartSysBase(const LU::SessionReaderSharedPtr session,
-                     const SD::MeshGraphSharedPtr graph,
-                     ParticleSpec particle_spec, MPI_Comm comm = MPI_COMM_WORLD,
-                     PartSysOptions options = PartSysOptions());
+  PartSysBase(const LU::SessionReaderSharedPtr session,
+              const SD::MeshGraphSharedPtr graph, ParticleSpec particle_spec,
+              MPI_Comm comm = MPI_COMM_WORLD,
+              PartSysOptions options = PartSysOptions());
 
   /// Object used to map to/from nektar geometry ids to 0,N-1
   std::shared_ptr<CellIDTranslation> cell_id_translation;
@@ -99,11 +99,20 @@ protected:
    *  @param args Remaining arguments (variable length) should be sym instances
    *  indicating which ParticleDats are to be written.
    */
-  template <typename... T>
-  inline void init_output(std::string fname, T... args);
+  template <typename... T> void init_output(std::string fname, T... args) {
+    if (this->h5part) {
+      if (this->sycl_target->comm_pair.rank_parent == 0) {
+        nprint("Ignoring (duplicate?) call to init_output().");
+      }
+    } else {
+      // Create H5Part instance
+      this->h5part =
+          std::make_shared<H5Part>(fname, this->particle_group, args...);
+    }
+  }
 
   /// @brief Read some parameters associated with all particle systems.
-  inline void read_params();
+  void read_params();
 
   /**
    * @brief Store particle param values in a map.Values are reported later via
@@ -111,7 +120,12 @@ protected:
    * @param label Label to attach to the parameter
    * @param value Value of the parameter that was set
    */
-  template <typename T> inline void report_param(std::string label, T val);
+  template <typename T> void report_param(std::string label, T val) {
+    // form stringstream and store string value in private map
+    std::stringstream ss;
+    ss << val;
+    param_vals_to_report[label] = ss.str();
+  }
 
 private:
   /// Output frequency read from config file
@@ -123,146 +137,5 @@ private:
   std::map<std::string, std::string> param_vals_to_report;
 };
 
-inline PartSysBase::PartSysBase(const LU::SessionReaderSharedPtr session,
-                                const SD::MeshGraphSharedPtr graph,
-                                ParticleSpec particle_spec, MPI_Comm comm,
-                                PartSysOptions options)
-    : session(session), graph(graph), comm(comm), h5part_exists(false),
-      ndim(graph->GetSpaceDimension()) {
-
-  read_params();
-
-  // Store options
-  this->options = options;
-
-  // Create interface between particles and nektar++
-  this->particle_mesh_interface =
-      std::make_shared<ParticleMeshInterface>(graph, 0, this->comm);
-  extend_halos_fixed_offset(this->options.extend_halos_offset,
-                            this->particle_mesh_interface);
-  this->sycl_target =
-      std::make_shared<SYCLTarget>(0, particle_mesh_interface->get_comm());
-  this->nektar_graph_local_mapper = std::make_shared<NektarGraphLocalMapper>(
-      this->sycl_target, this->particle_mesh_interface);
-  this->domain = std::make_shared<Domain>(this->particle_mesh_interface,
-                                          this->nektar_graph_local_mapper);
-
-  // Create ParticleGroup
-  this->particle_group =
-      std::make_shared<ParticleGroup>(domain, particle_spec, sycl_target);
-
-  // Set up map between cell indices
-  this->cell_id_translation = std::make_shared<CellIDTranslation>(
-      this->sycl_target, this->particle_group->cell_id_dat,
-      this->particle_mesh_interface);
-}
-
-/**
- * @details For each entry in the param_vals map (constructed via report_param),
- * write the value to stdout
- * @see also report_param()
- */
-inline void PartSysBase::add_params_report() {
-
-  std::cout << "Particle settings:" << std::endl;
-  for (auto const &[param_lbl, param_str_val] : this->param_vals_to_report) {
-    std::cout << "  " << param_lbl << ": " << param_str_val << std::endl;
-  }
-  std::cout << "============================================================="
-               "=========="
-            << std::endl
-            << std::endl;
-}
-
-inline void PartSysBase::free() {
-  if (this->h5part_exists) {
-    this->h5part->close();
-  }
-  this->particle_group->free();
-  this->sycl_target->free();
-  this->particle_mesh_interface->free();
-};
-
-template <typename... T>
-inline void PartSysBase::init_output(std::string fname, T... args) {
-  if (this->h5part_exists) {
-    if (this->sycl_target->comm_pair.rank_parent == 0) {
-      nprint("Ignoring (duplicate?) call to init_output().");
-    }
-  } else {
-    // Create H5Part instance
-    this->h5part =
-        std::make_shared<H5Part>(fname, this->particle_group, args...);
-    this->h5part_exists = true;
-  }
-}
-
-inline bool PartSysBase::is_output_step(int step) {
-  return this->output_freq > 0 && (step % this->output_freq) == 0;
-}
-
-inline void PartSysBase::read_params() {
-
-  // Read total number of particles / number per cell from config
-  int num_parts_per_cell, num_parts_tot;
-  this->session->LoadParameter(NUM_PARTS_TOT_STR, num_parts_tot, -1);
-  this->session->LoadParameter(NUM_PARTS_PER_CELL_STR, num_parts_per_cell, -1);
-
-  if (num_parts_tot > 0) {
-    this->num_parts_tot = num_parts_tot;
-    if (num_parts_per_cell > 0) {
-      nprint("Ignoring value of '" + NUM_PARTS_PER_CELL_STR +
-             "' because  "
-             "'" +
-             NUM_PARTS_TOT_STR + "' was specified.");
-    }
-  } else {
-    if (num_parts_per_cell > 0) {
-      // Determine the global number of elements
-      const int num_elements_local = this->graph->GetNumElements();
-      int num_elements_global;
-      MPICHK(MPI_Allreduce(&num_elements_local, &num_elements_global, 1,
-                           MPI_INT, MPI_SUM, this->comm));
-
-      // compute the global number of particles
-      this->num_parts_tot = ((int64_t)num_elements_global) * num_parts_per_cell;
-
-      report_param("Number of particles per cell/element", num_parts_per_cell);
-    } else {
-      nprint("Particles disabled (Neither '" + NUM_PARTS_TOT_STR +
-             "' or "
-             "'" +
-             NUM_PARTS_PER_CELL_STR + "' are set)");
-    }
-  }
-  report_param("Total number of particles", this->num_parts_tot);
-
-  // Output frequency
-  // ToDo Should probably be unsigned, but complicates use of LoadParameter
-  this->session->LoadParameter(PART_OUTPUT_FREQ_STR, this->output_freq, 0);
-  report_param("Output frequency (steps)", this->output_freq);
-}
-
-template <typename T>
-inline void PartSysBase::report_param(std::string label, T val) {
-  // form stringstream and store string value in private map
-  std::stringstream ss;
-  ss << val;
-  param_vals_to_report[label] = ss.str();
-}
-
-inline void PartSysBase::write(const int step) {
-  if (this->h5part_exists) {
-    if (this->sycl_target->comm_pair.rank_parent == 0) {
-      nprint("Writing particle properties at step", step);
-    }
-    this->h5part->write();
-  } else {
-    if (this->sycl_target->comm_pair.rank_parent == 0) {
-      nprint("Ignoring call to write particle data because an output file "
-             "wasn't set up. init_output() not called?");
-    }
-  }
-};
 } // namespace NESO::Particles
 #endif
