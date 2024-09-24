305a157fe4f87e3dbbc9a30ce8d42761cfc12f79
Will Saunders <will.saunders@ukaea.uk>
2024-09-24 15:07:25 +0100

    moved particle boundary conditions over to new style particle loop

--- include/nektar_interface/particle_boundary_conditions.hpp
+++ include/nektar_interface/particle_boundary_conditions.hpp
@@ -103,42 +103,29 @@ public:
    * ParticleDat this instance was created with.
    */
   inline void execute() {
-
     auto t0 = profile_timestamp();
-    auto pl_iter_range = this->position_dat->get_particle_loop_iter_range();
-    auto pl_stride = this->position_dat->get_particle_loop_cell_stride();
-    auto pl_npart_cell = this->position_dat->get_particle_loop_npart_cell();
     const int k_ndim = this->ndim;
-
     NESOASSERT(((k_ndim > 0) && (k_ndim < 4)), "Bad number of dimensions");
     const auto k_origin = this->d_origin.ptr;
     const auto k_extents = this->d_extents.ptr;
-    auto k_positions_dat = this->position_dat->cell_dat.device_ptr();
 
-    this->sycl_target->queue
-        .submit([&](sycl::handler &cgh) {
-          cgh.parallel_for<>(
-              sycl::range<1>(pl_iter_range), [=](sycl::id<1> idx) {
-                NESO_PARTICLES_KERNEL_START
-                const INT cellx = NESO_PARTICLES_KERNEL_CELL;
-                const INT layerx = NESO_PARTICLES_KERNEL_LAYER;
-                for (int dimx = 0; dimx < k_ndim; dimx++) {
-                  const double pos =
-                      k_positions_dat[cellx][dimx][layerx] - k_origin[dimx];
-                  // offset the position in the current dimension to be
-                  // positive by adding a value times the extent
-                  const double n_extent_offset_real = ABS(pos);
-                  const double tmp_extent = k_extents[dimx];
-                  const INT n_extent_offset_int = n_extent_offset_real + 2.0;
-                  const double pos_fmod =
-                      fmod(pos + n_extent_offset_int * tmp_extent, tmp_extent);
-                  k_positions_dat[cellx][dimx][layerx] =
-                      pos_fmod + k_origin[dimx];
-                }
-                NESO_PARTICLES_KERNEL_END
-              });
-        })
-        .wait_and_throw();
+    particle_loop(
+        "NektarCartesianPeriodic::execute", this->position_dat,
+        [=](auto k_positions_dat) {
+          for (int dimx = 0; dimx < k_ndim; dimx++) {
+            const REAL pos = k_positions_dat.at(dimx) - k_origin[dimx];
+            // offset the position in the current dimension to be
+            // positive by adding a value times the extent
+            const REAL n_extent_offset_real = ABS(pos);
+            const REAL tmp_extent = k_extents[dimx];
+            const INT n_extent_offset_int = n_extent_offset_real + 2.0;
+            const REAL pos_fmod =
+                sycl::fmod(pos + n_extent_offset_int * tmp_extent, tmp_extent);
+            k_positions_dat.at(dimx) = pos_fmod + k_origin[dimx];
+          }
+        },
+        Access::write(this->position_dat))
+        ->execute();
 
     sycl_target->profile_map.inc("NektarCartesianPeriodic", "execute", 1,
                                  profile_elapsed(t0, profile_timestamp()));
