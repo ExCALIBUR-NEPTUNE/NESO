5af9346c672a9798e4f82c6e519806be8090d4e9
Will Saunders <will.saunders@ukaea.uk>
2025-01-15 10:47:28 +0000

    removed alignment helper function that isnow unused

--- include/nektar_interface/utility_sycl.hpp
+++ include/nektar_interface/utility_sycl.hpp
@@ -111,19 +111,6 @@ template <typename T> inline T pad_to_vector_length(const T L) {
   }
 }
 
-/**
- * @param ptr Pointer to align to alignment.
- * @param alignment Power of two to align to.
- * @returns Aligned pointer.
- */
-template <typename T>
-constexpr inline T *neso_cast_align_pointer(void *ptr,
-                                            const std::size_t alignment) {
-  std::size_t ptr_int = reinterpret_cast<std::size_t>(ptr);
-  ptr_int = (ptr_int + (alignment - 1)) & -alignment;
-  return reinterpret_cast<T *>(ptr_int);
-}
-
 } // namespace NESO
 
 #endif
