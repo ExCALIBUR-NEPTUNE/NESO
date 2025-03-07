61c738c12b32f912ac3aad9faac3cbc4c456d7bd
Owen Parry <owen.parry@ukaea.uk>
2025-03-07 14:08:48 +0000

    Replace m_ with this-> throughout SolverTest.

--- test/common/SolverTest.cpp
+++ test/common/SolverTest.cpp
@@ -3,10 +3,10 @@
 
 void SolverTest::cancel_output_redirect() {
   // Restore stdout, stderr buffers
-  std::cout.rdbuf(m_orig_stdout_buf);
-  m_out_strm.close();
-  std::cerr.rdbuf(m_orig_stderr_buf);
-  m_err_strm.close();
+  std::cout.rdbuf(this->orig_stdout_buf);
+  this->out_strm.close();
+  std::cerr.rdbuf(this->orig_stderr_buf);
+  this->err_strm.close();
 }
 
 std::vector<std::string> SolverTest::assemble_args() const {
@@ -19,11 +19,11 @@ const ::testing::TestInfo *SolverTest::get_current_test_info() const {
 
 std::vector<std::string> SolverTest::assemble_default_args() const {
   std::filesystem::path config_fpath =
-      m_test_run_dir / (m_test_name + "_config.xml");
+      this->test_run_dir / (this->test_name + "_config.xml");
   std::filesystem::path mesh_fpath =
-      m_test_run_dir / (m_test_name + "_mesh.xml");
+      this->test_run_dir / (this->test_name + "_mesh.xml");
   std::vector<std::string> args;
-  args.push_back(m_solver_name);
+  args.push_back(this->solver_name);
   args.push_back(std::string(config_fpath));
   args.push_back(std::string(mesh_fpath));
   return args;
@@ -32,28 +32,28 @@ std::vector<std::string> SolverTest::assemble_default_args() const {
 void SolverTest::make_test_run_dir() const {
   if (is_root()) {
     // Remove any previous run dir
-    std::filesystem::remove_all(m_test_run_dir);
+    std::filesystem::remove_all(this->test_run_dir);
     // Create dir
-    std::filesystem::create_directories(m_test_run_dir.parent_path());
+    std::filesystem::create_directories(this->test_run_dir.parent_path());
 
     // Copy in common resources
-    if (std::filesystem::exists(m_common_test_res_dir)) {
+    if (std::filesystem::exists(this->common_test_res_dir)) {
       std::filesystem::copy(
-          m_common_test_res_dir, m_test_run_dir,
+          this->common_test_res_dir, this->test_run_dir,
           std::filesystem::copy_options::recursive |
               std::filesystem::copy_options::overwrite_existing);
     }
     // (Don't bother warning if a common resource directory doesn't exist)
 
     // Copy in test-specific resources
-    if (std::filesystem::exists(m_test_res_dir)) {
+    if (std::filesystem::exists(this->test_res_dir)) {
       std::filesystem::copy(
-          m_test_res_dir, m_test_run_dir,
+          this->test_res_dir, this->test_run_dir,
           std::filesystem::copy_options::recursive |
               std::filesystem::copy_options::overwrite_existing);
     } else {
       std::cout << "Skipping copy of test-specific resources; no directory at "
-                << m_test_res_dir << std::endl;
+                << this->test_res_dir << std::endl;
     }
   }
   // Other tasks wait for the dir to be created before continuuing
@@ -66,25 +66,25 @@ void SolverTest::make_test_run_dir() const {
 void SolverTest::print_preamble() const {
   std::cout << "Running Nektar solver test [" << get_current_test_info()->name()
             << "]";
-  std::cout << " in [" << m_test_run_dir << "]" << std::endl;
+  std::cout << " in [" << this->test_run_dir << "]" << std::endl;
   std::cout << "  Args: " << std::endl;
-  for (auto ii = 1; ii < m_args.size(); ii++) {
-    std::cout << "    " << m_args[ii] << std::endl;
+  for (auto ii = 1; ii < this->args.size(); ii++) {
+    std::cout << "    " << this->args[ii] << std::endl;
   }
   std::cout << std::endl;
 }
 
 void SolverTest::redirect_output_to_file() {
   // Save current buffers
-  m_orig_stdout_buf = std::cout.rdbuf();
-  m_orig_stderr_buf = std::cerr.rdbuf();
+  this->orig_stdout_buf = std::cout.rdbuf();
+  this->orig_stderr_buf = std::cerr.rdbuf();
   // Redirect
   std::string stdout_fname = "stdout." + std::to_string(get_rank()) + ".txt";
   std::string stderr_fname = "stderr." + std::to_string(get_rank()) + ".txt";
-  m_out_strm.open(m_test_run_dir / stdout_fname);
-  std::cout.rdbuf(m_out_strm.rdbuf());
-  m_err_strm.open(m_test_run_dir / stderr_fname);
-  std::cerr.rdbuf(m_err_strm.rdbuf());
+  this->out_strm.open(this->test_run_dir / stdout_fname);
+  std::cout.rdbuf(this->out_strm.rdbuf());
+  this->err_strm.open(this->test_run_dir / stderr_fname);
+  std::cerr.rdbuf(this->err_strm.rdbuf());
 }
 
 int SolverTest::run(MainFuncType func, bool redirect_output) {
@@ -96,16 +96,16 @@ int SolverTest::run(MainFuncType func, bool redirect_output) {
   // relative to cwd, rather than to the session file itself - the Nektar docs
   // are wrong...
   std::filesystem::path old_dir = std::filesystem::current_path();
-  std::filesystem::current_path(m_test_run_dir);
+  std::filesystem::current_path(this->test_run_dir);
 
-  m_args = assemble_args();
+  this->args = assemble_args();
 
   // Construct argv, argc
   std::vector<char *> cstr_args{};
-  for (auto &arg : m_args)
+  for (auto &arg : this->args)
     cstr_args.push_back(&arg.front());
-  m_argv = cstr_args.data();
-  m_argc = cstr_args.size();
+  this->argv = cstr_args.data();
+  this->argc = cstr_args.size();
 
   // Redirect stdout, stderr if requested
   if (redirect_output) {
@@ -115,7 +115,7 @@ int SolverTest::run(MainFuncType func, bool redirect_output) {
   print_preamble();
 
   // Run the solver
-  int solver_ret_code = func(m_argc, m_argv);
+  int solver_ret_code = func(this->argc, this->argv);
 
   // Restore stdout, stderr if they were redirected
   if (redirect_output) {
@@ -137,16 +137,17 @@ SolverTest::get_test_run_dir(const std::string &solver_name,
 
 void SolverTest::SetUp() {
   // Set solver name (allowing derived classes to override)
-  m_solver_name = get_solver_name();
+  this->solver_name = get_solver_name();
   // Set test name
-  m_test_name = get_current_test_info()->name();
+  this->test_name = get_current_test_info()->name();
 
   // Determine test resource locations
-  m_common_test_res_dir = get_common_test_resources_dir(m_solver_name);
-  m_test_res_dir = get_test_resources_dir(m_solver_name, m_test_name);
+  this->common_test_res_dir = get_common_test_resources_dir(this->solver_name);
+  this->test_res_dir =
+      get_test_resources_dir(this->solver_name, this->test_name);
 
   // Set test run location
-  m_test_run_dir = get_test_run_dir(m_solver_name, m_test_name);
+  this->test_run_dir = get_test_run_dir(this->solver_name, this->test_name);
 
   additional_setup_tasks();
 }
