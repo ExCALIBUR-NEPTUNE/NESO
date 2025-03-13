6537b0e007a24b84fec03cf6622b21e0e0ebf842
Owen Parry <owen.parry@ukaea.uk>
2025-03-13 09:08:25 +0000

    Disable with-particles tests and Doxygen all regression tests.

--- test/regression/examples/DriftPlane/test_DriftPlane.cpp
+++ test/regression/examples/DriftPlane/test_DriftPlane.cpp
@@ -4,13 +4,13 @@
 
 namespace NESO::Solvers {
 
+/**
+ * @brief Regression tests for the DriftPlane solver examples. Note that test
+ * names are used to determine the location of the corresponding example
+ * directories and hence the config and mesh files.
+ */
 class DriftPlaneRegTest : public SolverRegTest {};
 
-/**
- * Regression tests for the DriftPlane solver examples. Note that the test name
- * itself is used to determine the location of the corresponding example
- * directory, config file and mesh file.
- */
 TEST_F(DriftPlaneRegTest, blob2d) { run_and_regress(); }
 
 } // namespace NESO::Solvers
--- test/regression/examples/H3LAPD/test_H3LAPD.cpp
+++ test/regression/examples/H3LAPD/test_H3LAPD.cpp
@@ -4,24 +4,32 @@
 
 namespace NESO::Solvers {
 
-class H3LAPDRegTest : public SolverRegTest {};
-
 /**
- * Regression tests for the H3LAPD solver examples. Note that the test name
- * itself is used to determine the location of the corresponding example
- * directory, config file and mesh file.
+ * @brief Regression tests for the H3LAPD solver examples. Note that test names
+ * are used to determine the location of the corresponding example directories
+ * and hence the config and mesh files.
  */
-TEST_F(H3LAPDRegTest, 2DRogersRicci) { run_and_regress(); }
+class H3LAPDRegTest : public SolverRegTest {};
 
 TEST_F(H3LAPDRegTest, 2DHW) { run_and_regress(); }
 
-TEST_F(H3LAPDRegTest, 3DHW) {
-  set_tolerance(1e-9);
+/**
+Stochasticity from particles isn't accounted for yet, disabling all
+with-particles tests for now
+TEST_F(H3LAPDRegTest, 2Din3DHW) {
   run_and_regress();
 }
+ */
 
 TEST_F(H3LAPDRegTest, 2Din3DHW_fluid_only) {
   set_tolerance(1e-7);
+  run_and_regress();
+}
+
+TEST_F(H3LAPDRegTest, 2DRogersRicci) { run_and_regress(); }
+
+TEST_F(H3LAPDRegTest, 3DHW) {
+  set_tolerance(1e-9);
   run_and_regress();
 }
 
